18da8c04c52bb1b37f33e769374b504e675d8d56
Nok Wongpiromsarn <tichakorn@gmail.com>
2010-08-12 06:42:40 +0000

    In jtlvint, added an option for file handling. Updated the tutorial.

--- trunk/jtlvint.py
+++ trunk/jtlvint.py
@@ -42,7 +42,7 @@ def setJTLVExe(jtlv_exe):
 def generateJTLVInput(env_vars={}, disc_sys_vars={}, spec='', disc_props={}, \
                           disc_dynamics=PropPreservingPartition(), \
                           smv_file='specs/spec.smv', spc_file='specs/spec.spc', \
-                          verbose=0):
+                          file_exist_option='a', verbose=0):
     """Generate JTLV input files: smv_file and spc_file.
 
     Input:
@@ -60,6 +60,11 @@ def generateJTLVInput(env_vars={}, disc_sys_vars={}, spec='', disc_props={}, \
       on discrete variables.
     - `disc_dynamics`: a PropPreservingPartition object that represents the 
       transition system obtained from the discretization procedure.
+    - `smv_file`: a string that specifies the name of the resulting smv file.
+    - `spc_file`: a string that specifies the name of the resulting spc file.
+    - `file_exist_option`: a string that indicate what to do when the specified smv_file 
+      or spc_file exists. Possible values are: 'a' (ask whether to replace or
+      create a new file), 'r' (replace the existing file), 'n' (create a new file).
     - `verbose`: an integer that specifies the verbose level. If verbose is set to 0,
       this function will not print anything on the screen.
     """
@@ -87,11 +92,11 @@ def generateJTLVInput(env_vars={}, disc_sys_vars={}, spec='', disc_props={}, \
         printError("The input spc_file is expected to be a string")
         raise TypeError("Invalid spc_file.")
 
-    # Figure out the names of the smv and spc files
-    if (smv_file[-4:] != '.smv'):
-        smv_file = smv_file + '.smv'
-    if (spc_file[-4:] != '.spc'):
-        spc_file = spc_file + '.spc'
+#    # Figure out the names of the smv and spc files
+#     if (smv_file[-4:] != '.smv'):
+#         smv_file = smv_file + '.smv'
+#     if (spc_file[-4:] != '.spc'):
+#         spc_file = spc_file + '.spc'
 
     if (not os.path.exists(os.path.abspath(os.path.dirname(smv_file)))):
         printWarning('Folder for smv_file ' + smv_file + ' does not exist. Creating...')
@@ -99,6 +104,43 @@ def generateJTLVInput(env_vars={}, disc_sys_vars={}, spec='', disc_props={}, \
     if (not os.path.exists(os.path.abspath(os.path.dirname(spc_file)))):
         printWarning('Folder for spc_file ' + spc_file + ' does not exist. Creating...')
         os.mkdir(os.path.abspath(os.path.dirname(spc_file)))
+
+    # Check whether the smv or spc file exists
+    if (file_exist_option != 'r'):
+        if (os.path.exists(smv_file)):
+            printWarning('smv file: ' + smv_file + ' exists.')
+            smv_file_exist_option = file_exist_option
+            while (smv_file_exist_option.lower() != 'r' and \
+                       smv_file_exist_option.lower() != 'n'):
+                smv_file_exist_option = raw_input('Replace [r] or create a new smv file [n]: ')
+            if (smv_file_exist_option.lower() == 'n'):
+                i = 1
+                smv_file_part = smv_file.partition('.')
+                smv_file = smv_file_part[0] + str(i) + smv_file_part[1] + \
+                    smv_file_part[2]
+                while (os.path.exists(smv_file)):
+                    i = i + 1
+                    smv_file = smv_file_part[0] + str(i) + smv_file_part[1] + \
+                        smv_file_part[2]
+                print('smv file: ' + smv_file)
+
+        if (os.path.exists(spc_file)):
+            printWarning('spc file: ' + spc_file + ' exists.')
+            spc_file_exist_option = file_exist_option
+            while (spc_file_exist_option.lower() != 'r' and \
+                       spc_file_exist_option.lower() != 'n'):
+                spc_file_exist_option = raw_input('Replace [r] or create a new spc file [n]: ')
+            if (spc_file_exist_option.lower() == 'n'):
+                i = 1
+                spc_file_part = spc_file.partition('.')
+                spc_file = spc_file_part[0] + str(i) + spc_file_part[1] + \
+                    spc_file_part[2]
+                while (os.path.exists(spc_file)):
+                    i = i + 1
+                    spc_file = spc_file_part[0] + str(i) + spc_file_part[1] + \
+                        spc_file_part[2]
+                print('spc file: ' + spc_file)
+            
 
     ###################################################################################
     # Generate smv file
@@ -150,8 +192,8 @@ def generateJTLVInput(env_vars={}, disc_sys_vars={}, spec='', disc_props={}, \
         if ('boolean' in reg):
             continue
         elif (isinstance(reg, str)):
-            dots_values = re.findall('([-+]?\d+)\s*?,?\s*?' + r'\.\.\.' + \
-                                         '\s*?,?\s*?([-+]?\d+)', reg)
+            dots_values = re.findall('([-+]?\d+)\s*,?\s*' + r'\.\.\.' + \
+                                         '\s*,?\s*([-+]?\d+)', reg)
             all_values = list(set(re.findall('[-+]?\d+', reg)))
             if (len(all_values) > 0):
                 for dots_pair in dots_values:
@@ -390,15 +432,15 @@ def generateJTLVInput(env_vars={}, disc_sys_vars={}, spec='', disc_props={}, \
 ###################################################################
 
 def checkRealizability(smv_file='', spc_file='', aut_file='', heap_size='-Xmx128m', \
-                           pick_sys_init=True, verbose=0):
+                           pick_sys_init=True, file_exist_option='a', verbose=0):
     """Determine whether the spec in smv_file and spc_file is realizable without 
     extracting an automaton.
 
     Input:
 
-    - `smv_file`: a string containing the name of the smv file.
-    - `spc_file`: a string containing the name of the spc file.
-    - `aut_file`: a string containing the name of the file containing the output of JTLV
+    - `smv_file`: a string that specifies the name of the smv file.
+    - `spc_file`: a string that specifies the name of the spc file.
+    - `aut_file`: a string that specifies the name of the file containing the output of JTLV
       (e.g. an initial state starting from which the spec is cannot be satisfied).
     - `jtlv_path`: a string containing the full path to the JTLV folder.
     - `heap_size`: a string that specifies java heap size. 
@@ -406,6 +448,10 @@ def checkRealizability(smv_file='', spc_file='', aut_file='', heap_size='-Xmx128
       this function will not print anything on the screen.
     - `pick_sys_init` is a boolean indicating whether the system can pick 
       its initial state (in response to the initial environment state).
+    - `file_exist_option`: a string that indicate what to do when the specified aut_file 
+      exists. Possible values are: 'a' (ask whether to replace or create a new file), 
+      'r' (replace the existing file), 'n' (create a new file).
+    - `verbose`: an integer that specifies the verbose level.
     """
 
     init_option = 1
@@ -413,27 +459,83 @@ def checkRealizability(smv_file='', spc_file='', aut_file='', heap_size='-Xmx128
         init_option = 0
     realizable = computeStrategy(smv_file=smv_file, spc_file=spc_file, \
                                      aut_file=aut_file, heap_size=heap_size, \
-                                     priority_kind=-1, verbose=verbose, \
-                                     init_option=init_option)
+                                     priority_kind=-1, init_option=init_option, \
+                                     file_exist_option=file_exist_option, verbose=verbose)
     return realizable
+
 
 
 ###################################################################
 
-def computeStrategy(smv_file='', spc_file='', aut_file='', heap_size='-Xmx128m', \
-                        priority_kind=3, init_option=1, verbose=0):
+def synthesize(env_vars={}, disc_sys_vars={}, spec='', disc_props={}, \
+                   disc_dynamics=PropPreservingPartition(), \
+                   smv_file='specs/spec.smv', spc_file='specs/spec.spc', \
+                   aut_file='', heap_size='-Xmx128m', priority_kind=3, init_option=1, \
+                   file_exist_option='a', verbose=0):
+    """Compute an automaton satisfying `spec`. Return the realizability of `spec`.
+    If `spec` is realizable, the resulting automaton will be stored in the
+    `aut_file`. Otherwise, the counter examples will be stored.
+    This function essentially combines ``generateJTLVInput`` and ``computeStrategy``
+
+    Input:
+
+    - `env_vars`: a dictionary {str : str} or {str : list} whose keys are the names 
+      of environment variables and whose values are their possible values, e.g., 
+      boolean or {0, 2, ..., 5} or [0, 2, 3, 4, 5].
+    - `disc_sys_vars`: a dictionary {str : str} or {str : list} whose keys are the 
+      names of discrete system variables and whose values are their possible values.
+    - `spec`: a list of two strings that represents system specification of the form
+      assumption -> guarantee; the first string is the assumption and the second 
+      string is the guarantee.
+    - `disc_props`: a dictionary {str : str} whose keys are the symbols for 
+      propositions on discrete variables and whose values are the actual propositions
+      on discrete variables.
+    - `disc_dynamics`: a PropPreservingPartition object that represents the 
+      transition system obtained from the discretization procedure.
+    - `smv_file`: a string that specifies the name of the resulting smv file.
+    - `spc_file`: a string that specifies the name of the resulting spc file.
+    - `aut_file`: a string that specifies the name of the file containing the resulting 
+      automaton.
+    - `heap_size`: a string that specifies java heap size. 
+    - `priority_kind`: a string of length 3 or an integer that specifies the type of 
+      priority used in extracting the automaton. See the documentation of the 
+      ``computeStrategy`` function for the possible values of `priority_kind`.
+    - `init_option`: an integer in that specifies how to handle the initial state of 
+      the system. See the documentation of the ``computeStrategy`` function for the 
+      possible values of `init_option`.
+    - `file_exist_option`: a string that indicate what to do when the specified smv_file 
+      or spc_file exists. Possible values are: 'a' (ask whether to replace or
+      create a new file), 'r' (replace the existing file), 'n' (create a new file).
+    - `verbose`: an integer that specifies the verbose level. If verbose is set to 0,
+      this function will not print anything on the screen.
+    """
+    generateJTLVInput(env_vars=env_vars, disc_sys_vars=disc_sys_vars, spec=spec, \
+                          disc_props=disc_props, disc_dynamics=disc_dynamics, \
+                          smv_file=smv_file, spc_file=spc_file, \
+                          file_exist_option=file_exist_option, verbose=verbose)
+    realizability = computeStrategy(smv_file=smv_file, spc_file=spc_file, \
+                                        aut_file=aut_file, heap_size=heap_size, \
+                                        priority_kind=priority_kind, \
+                                        init_option=init_option, \
+                                        file_exist_option=file_exist_option, \
+                                        verbose=verbose)
+    return realizability
+
+
+###################################################################
+
+def computeStrategy(smv_file, spc_file, aut_file='', heap_size='-Xmx128m', \
+                        priority_kind=3, init_option=1, file_exist_option='a', verbose=0):
     """Compute an automaton satisfying the spec in smv_file and spc_file and store in 
     aut_file. Return the realizability of the spec.
 
     Input:
 
-    - `smv_file`: a string containing the name of the smv file.
-    - `spc_file`: a string containing the name of the spc file.
-    - `aut_file`: a string containing the name of the file containing the resulting 
+    - `smv_file`: a string that specifies the name of the smv file.
+    - `spc_file`: a string that specifies the name of the spc file.
+    - `aut_file`: a string that specifies the name of the file containing the resulting 
       automaton.
     - `heap_size`: a string that specifies java heap size. 
-    - `verbose`: an integer that specifies the verbose level. If verbose is set to 0, 
-      this function will not print anything on the screen.
     - `priority_kind`: a string of length 3 or an integer that specifies the type of 
       priority used in extracting the automaton. Possible values of `priority_kind` are: 
 
@@ -461,6 +563,10 @@ def computeStrategy(smv_file='', spc_file='', aut_file='', heap_size='-Xmx128m',
           environment state. For each initial environment state, the resulting
           automaton contain all the possible initial system states, starting from which
           the system can satisfy the specification.
+    - `file_exist_option`: a string that indicate what to do when the specified aut_file 
+      exists. Possible values are: 'a' (ask whether to replace or create a new file), 
+      'r' (replace the existing file), 'n' (create a new file).
+    - `verbose`: an integer that specifies the verbose level.
     """
 
     # Check that the input is valid
@@ -476,9 +582,29 @@ def computeStrategy(smv_file='', spc_file='', aut_file='', heap_size='-Xmx128m',
     if (len(aut_file) == 0 or aut_file.isspace()):
         aut_file = re.sub(r'\.'+'[^'+r'\.'+']+$', '', spc_file)
         aut_file = aut_file + '.aut'
+        print('aut file: ' + aut_file)
     if (not os.path.exists(os.path.abspath(os.path.dirname(aut_file)))):
         printWarning('Folder for aut_file ' + aut_file + ' does not exist. Creating...')
         os.mkdir(os.path.abspath(os.path.dirname(aut_file)))
+
+    # Check whether the aut file exists
+    if (file_exist_option != 'r'):
+        if (os.path.exists(aut_file)):
+            printWarning('aut file: ' + aut_file + ' exists.')
+            aut_file_exist_option = file_exist_option
+            while (aut_file_exist_option.lower() != 'r' and \
+                       aut_file_exist_option.lower() != 'n'):
+                aut_file_exist_option = raw_input('Replace [r] or create a new aut file [n]: ')
+            if (aut_file_exist_option.lower() == 'n'):
+                i = 1
+                aut_file_part = aut_file.partition('.')
+                aut_file = aut_file_part[0] + str(i) + aut_file_part[1] + \
+                    aut_file_part[2]
+                while (os.path.exists(aut_file)):
+                    i = i + 1
+                    aut_file = aut_file_part[0] + str(i) + aut_file_part[1] + \
+                        aut_file_part[2]
+                print('aut file: ' + aut_file)
 
     # Convert the priority_kind to the corresponding integer
     if (isinstance(priority_kind, str)):
@@ -661,7 +787,8 @@ if __name__ == "__main__":
     pick_sys_init = (not ('2' in sys.argv))
     realizability = checkRealizability(smv_file=smvfile, spc_file=spcfile, \
                                            aut_file='', heap_size='-Xmx128m', \
-                                           pick_sys_init=pick_sys_init, verbose=3)
+                                           pick_sys_init=pick_sys_init, \
+                                           file_exist_option='a', verbose=3)
     print realizability
     print('DONE')
     print('================================\n')
@@ -676,10 +803,23 @@ if __name__ == "__main__":
         init_option = 0
     realizability = computeStrategy(smv_file=smvfile, spc_file=spcfile, aut_file='', \
                                         heap_size='-Xmx128m', priority_kind='ZYX', \
-                                        init_option=init_option, verbose=3)
+                                        init_option=init_option, file_exist_option='a', \
+                                        verbose=3)
     print realizability
     print('DONE')
     print('================================\n')
+
+    ####################################
+
+    print('Testing synthesize')
+
+    realizability = synthesize(env_vars=env_vars, disc_sys_vars=disc_sys_vars, \
+                                   spec=spec, disc_props=disc_props, \
+                                   disc_dynamics=disc_dynamics, \
+                                   smv_file=smvfile, spc_file=spcfile, \
+                                   aut_file='', heap_size='-Xmx128m', priority_kind=3, \
+                                   init_option=init_option, \
+                                   file_exist_option='a', verbose=3)
 
     ####################################
 
