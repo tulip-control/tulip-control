9eb3d30d7bdfb121a47e7406da1f35d00fe683ee
Scott Livingston <slivingston@caltech.edu>
2012-06-25 01:59:18 +0000

    Bump version in preparation for my gridworld code and Epydoc support.

--- setup.py
+++ setup.py
@@ -187,7 +187,7 @@ if check_deps:
 
 if perform_setup:
     setup(name = 'tulip',
-          version = '0.3b',
+          version = '0.3c',
           description = 'Temporal Logic Planning (TuLiP) Toolbox',
           author = 'Caltech Control and Dynamical Systems',
           author_email = 'murray@cds.caltech.edu',
