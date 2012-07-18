127e3369418f7aeb88e33cfceecdc5a1c8b937dd
Clemens Wiltsche <cw395@cantab.net>
2012-07-18 17:36:23 +0000

    Added parallelsim.py module to trunk/tulip/tulip for simulation of several automata in parallel. Further, added example_parallelsim.py to trunk/tulip/examples as an example of how to use the parallelsim module.

---
+++ examples/example_parallelsim.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+"""
+Example of parallel simulation: Executing a communication protocol to transmit integer data
+
+Clemens Wiltsche (wclemens@cds.caltech.edu)
+18 Jul 2012
+"""
+
+#@import_section@
+import sys, os
+
+from tulip import jtlvint, automaton
+#@import_section_end@
+
+
+########
+# Sender
+########
+
+# Specify where the smv file, spc file and aut file will go
+#@filename_section@
+sendfile = 'send'
+path = os.path.abspath(os.path.dirname(sys.argv[0]))
+smvfile = os.path.join(path, 'specs', sendfile+'.smv')
+spcfile = os.path.join(path, 'specs', sendfile+'.spc')
+autfile = os.path.join(path, 'specs', sendfile+'.aut')
+#@filename_section_end@
+
+#@specification_section@
+data = 3 # the maximum value that could be transmitted
+
+env_vars = {'ack' : 'boolean'}
+sys_disc_vars = {'req' : range(0,data)}
+# empty dynamics - not needed for this example
+disc_dynamics = None
+
+#GR1 specification
+assumption = '[](req=0 -> <>(!ack))\n'
+assumption += ' & [](ack & next(!ack) -> req=0)\n'
+assumption += ' & [](!ack & next(ack) -> req!=0)\n'
+
+guarantee = '[](ack -> next(req=-1))\n' # VI
+guarantee += ' & [](!ack -> next(req=1 | req=2))\n'
+guarantee += ' & [](req!=-1 & next(req=0) -> ack)\n'
+guarantee += ' & [](req=-1 & next(req!=0) -> !ack)\n'
+for d in xrange(1,data):
+    guarantee += ' & [](req=%i & next(req!=%i) -> next(req=0))\n' % (d, d) #V
+for d in xrange(1,data):
+    guarantee += ' & [](req=%i & !ack -> next(req=%i))\n' % (d, d) # IV
+#@specification_section_end@
+
+# Generate JTLV input
+#@geninput@
+prob = jtlvint.generateJTLVInput(env_vars, sys_disc_vars, [assumption, guarantee], \
+                                   {}, disc_dynamics, smvfile, spcfile, file_exist_option='r', verbose=2)
+#@eninout_end@
+
+# Construct automaton
+#@compaut@
+jtlvint.computeStrategy(smv_file=smvfile, spc_file=spcfile, aut_file=autfile, \
+                                    priority_kind=3, init_option=1, heap_size='-Xmx2g', file_exist_option='r', verbose=3)
+sender_aut = automaton.Automaton(autfile, [], 3)
+#@compaut_end@
+
+
+########
+# Receiver
+########
+
+#@filename_section@
+recvfile = 'recv'
+path = os.path.abspath(os.path.dirname(sys.argv[0]))
+smvfile = os.path.join(path, 'specs', recvfile+'.smv')
+spcfile = os.path.join(path, 'specs', recvfile+'.spc')
+autfile = os.path.join(path, 'specs', recvfile+'.aut')
+#@filename_section_end@
+
+#@specification_section@
+env_vars = {'req' : range(0,data)}
+sys_disc_vars = {'ack' : 'boolean', 'sink' : range(0,data)}
+# Empty dynamics
+disc_dynamics = None
+
+# GR1 specification
+assumption = '[](req!=0 & next(req=0) -> ack)\n' #X
+assumption += ' & [](req=0 & next(req!=0) -> !ack)\n' #IX
+
+guarantee = '[](req=0 -> next(!ack))\n' # XII
+guarantee += ' & [](req!=0 -> next(ack))\n' # XI
+guarantee += ' & [](!ack & next(ack) -> req!=0)\n' # XIV
+guarantee += ' & [](ack & next(!ack) -> req=0)\n' # XIII
+guarantee += ' & [](req!=0 -> <>('
+for d in xrange(1,data):
+    if d != 1:
+        guarantee += ' | '
+    guarantee += '(sink=%i & req=%i)' % (d, d)
+guarantee += '))\n' #XV
+#@specification_section_end@
+
+# Generate JTLV input
+#@geninput@
+prob = jtlvint.generateJTLVInput(env_vars, sys_disc_vars, [assumption, guarantee], \
+                                   {}, disc_dynamics, smvfile, spcfile, file_exist_option='r', verbose=2)
+#@geninput_end@
+
+# Construct automaton
+#@compaut@
+jtlvint.computeStrategy(smv_file=smvfile, spc_file=spcfile, aut_file=autfile, \
+                                    priority_kind=3, init_option=1, heap_size='-Xmx2g', file_exist_option='r', verbose=3)
+
+receiver_aut = automaton.Automaton(autfile, [], 3)
+#@compaut_end@
+
+
+#####################
+# Parallel simulation
+#####################
+
+#@sim@
+from tulip.parallelsim import Strategy
+
+# Initial values of variables
+V = {'ack': 0, 'req': 0, 'sink': 0}
+
+# Strategies
+sender_strat = Strategy(sender_aut, V, ['ack'], ['req'], 'sender')
+receiver_strat = Strategy(receiver_aut, V, ['req'], ['ack', 'sink'], 'receiver')
+
+# Start threads
+sender_strat.start()
+receiver_strat.start()
+
+#@sim_end@
+
+
---
+++ tulip/parallelsim.py
@@ -0,0 +1,129 @@
+# !/usr/bin/env python
+#
+# Copyright (c) 2011 by California Institute of Technology
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the California Institute of Technology nor
+#    the names of its contributors may be used to endorse or promote
+#    products derived from this software without specific prior
+#    written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CALTECH
+# OR THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+# 
+""" 
+----------------
+Parallel Simulation Module
+----------------
+"""
+
+import sys
+import time, random
+import threading
+
+from tulip import *
+from tulip.automaton import AutomatonState
+from errorprint import printError
+
+
+class Strategy(threading.Thread):
+    """
+    Simulate an single automaton as a strategy
+    
+    Several Strategy-threads can be executed in parallel to simulate
+    an asynchronous composition of automata. Shared variables are used to
+    communicate.
+
+    The output is only to stdout as formatted prints.
+    
+    Arguments to __init__ constructor:
+
+    - `D` -- an Automaton object generated from the jtlvint.synthesize or
+        jtlv.computeStrategy function.
+    - `V` -- a dictionary in which the keys correspond to variable names and the
+        values are the corresponding valuations. All variables that are either
+        written or read by the automaton associated with this Strategy must be
+        included in this dictionary. Note that if this Strategy is to be executed
+        in Parallel with other Strategies, then they must all be assigned the
+        same dictionary, as this is how shared variables are implemented.
+    - `X` -- a list of variable names. The environment variables of the
+        automaton. All variable names in `X` must be keys in `V`.
+    - `Y` -- a list of variable names. The system variables of the
+        automaton. All variable names in `Y` must be keys in `V`.
+    - `name` -- name to identify the Strategy in the screen output.
+    - `Tmin` -- minimum time between transitions (in ms)
+    - `Tmax` -- maximum time between transitions (in ms)
+        
+    Note: The automaton sleeps a certain amount of time between transitions.
+           This time is a value chosen uniformly between `Tmin` and `Tmax`.
+    """
+
+    def __init__(self, D, V, X, Y, name, Tmin=1000, Tmax=1000):
+        threading.Thread.__init__(self)
+        self.D = D # automaton
+        self.V = V # dictionary of state variables and their current values
+        self.X = X # list of names for environment variables
+        self.Y = Y # list of names for system variables
+
+        self.name = name # name to identify the automaton's ID
+
+        self.Tmin = Tmin # minimum time between steps (in ms)
+        self.Tmax = Tmax # maximum time between steps (in ms)
+
+
+    # Inherit run function from Thread class
+    def run(self):
+        # INITIALITY
+        # get environment variables
+        s0 = dict(filter(lambda (k,v): k in (self.X + self.Y), self.V.iteritems()))
+
+        # apply inputs
+        aut_state = self.D.findNextAutState(current_aut_state=None, env_state=s0)
+
+        # check if initial conditions satisfy assumptions
+        if (aut_state==-1):
+            printError(self.name + ": No transition found.", obj=self)
+
+        # CONSECUTION
+        while True:
+
+            print '%s %04i\t: %s\n' % (self.name, aut_state.id, str(self.V)),
+
+            #sleep for a given time (to emulate different processor frequencies)
+            time.sleep(random.uniform(self.Tmin/1000, self.Tmax/1000)) 
+
+            # get environment variables
+            inputs = dict(filter(lambda (k,v): k in self.X, self.V.iteritems()))
+
+            # apply inputs
+            aut_state = self.D.findNextAutState(current_aut_state=aut_state, env_state=inputs)
+            if (aut_state==-1):
+                printError(self.name + ": No transition found.", obj=self)
+                
+            # check environment assumptions
+            if(not isinstance(aut_state, AutomatonState)):
+                printError(self.name + ": The environment violated its assumptions.", obj=self)
+                    
+            # write system variables
+            map(lambda (k,v): self.V.update({k:v}), dict(filter(lambda (k,v): k in self.Y, aut_state.state.iteritems())).iteritems())
