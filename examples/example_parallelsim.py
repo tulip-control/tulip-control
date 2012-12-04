d45a37a49d67dd9ba1b213f29a91ab27109f978d
Scott Livingston <slivingston@caltech.edu>
2012-12-03 16:15:52 -0800

    EXAMPLE DOC: remove unused Sphinx tags

--- examples/example_parallelsim.py
+++ examples/example_parallelsim.py
@@ -2,15 +2,15 @@
 """
 Example of parallel simulation: Executing a communication protocol to transmit integer data
 
+
 Clemens Wiltsche (wclemens@cds.caltech.edu)
 18 Jul 2012
+
+Minor modifications by SCL; 3 Dec 2012.
 """
 
-#@import_section@
 import sys, os
-
 from tulip import jtlvint, automaton
-#@import_section_end@
 
 
 ########
@@ -18,15 +18,12 @@ from tulip import jtlvint, automaton
 ########
 
 # Specify where the smv file, spc file and aut file will go
-#@filename_section@
 sendfile = 'send'
 path = os.path.abspath(os.path.dirname(sys.argv[0]))
 smvfile = os.path.join(path, 'specs', sendfile+'.smv')
 spcfile = os.path.join(path, 'specs', sendfile+'.spc')
 autfile = os.path.join(path, 'specs', sendfile+'.aut')
-#@filename_section_end@
 
-#@specification_section@
 data = 3 # the maximum value that could be transmitted
 
 env_vars = {'ack' : 'boolean'}
@@ -47,35 +44,27 @@ for d in xrange(1,data):
     guarantee += ' & [](req=%i & next(req!=%i) -> next(req=0))\n' % (d, d) #V
 for d in xrange(1,data):
     guarantee += ' & [](req=%i & !ack -> next(req=%i))\n' % (d, d) # IV
-#@specification_section_end@
 
 # Generate JTLV input
-#@geninput@
 prob = jtlvint.generateJTLVInput(env_vars, sys_disc_vars, [assumption, guarantee], \
                                    {}, disc_dynamics, smvfile, spcfile, file_exist_option='r', verbose=2)
-#@eninout_end@
 
 # Construct automaton
-#@compaut@
 jtlvint.computeStrategy(smv_file=smvfile, spc_file=spcfile, aut_file=autfile, \
                                     priority_kind=3, init_option=1, heap_size='-Xmx2g', file_exist_option='r', verbose=3)
 sender_aut = automaton.Automaton(autfile, [], 3)
-#@compaut_end@
 
 
 ########
 # Receiver
 ########
 
-#@filename_section@
 recvfile = 'recv'
 path = os.path.abspath(os.path.dirname(sys.argv[0]))
 smvfile = os.path.join(path, 'specs', recvfile+'.smv')
 spcfile = os.path.join(path, 'specs', recvfile+'.spc')
 autfile = os.path.join(path, 'specs', recvfile+'.aut')
-#@filename_section_end@
 
-#@specification_section@
 env_vars = {'req' : range(0,data)}
 sys_disc_vars = {'ack' : 'boolean', 'sink' : range(0,data)}
 # Empty dynamics
@@ -95,28 +84,22 @@ for d in xrange(1,data):
         guarantee += ' | '
     guarantee += '(sink=%i & req=%i)' % (d, d)
 guarantee += '))\n' #XV
-#@specification_section_end@
 
 # Generate JTLV input
-#@geninput@
 prob = jtlvint.generateJTLVInput(env_vars, sys_disc_vars, [assumption, guarantee], \
                                    {}, disc_dynamics, smvfile, spcfile, file_exist_option='r', verbose=2)
-#@geninput_end@
 
 # Construct automaton
-#@compaut@
 jtlvint.computeStrategy(smv_file=smvfile, spc_file=spcfile, aut_file=autfile, \
                                     priority_kind=3, init_option=1, heap_size='-Xmx2g', file_exist_option='r', verbose=3)
 
 receiver_aut = automaton.Automaton(autfile, [], 3)
-#@compaut_end@
 
 
 #####################
 # Parallel simulation
 #####################
 
-#@sim@
 from tulip.parallelsim import Strategy
 
 # Initial values of variables
@@ -140,4 +123,3 @@ try:
 except KeyboardInterrupt:
     sender_strat.runtime = 1
     receiver_strat.runtime = 1
-#@sim_end@
