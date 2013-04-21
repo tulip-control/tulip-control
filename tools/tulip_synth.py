b22f037dfc330c31dafd9b75e03d5c65b4d4aaf5
Necmiye Ozay <necmiye.ozay@gmail.com>
2013-04-21 02:12:39 -0700

    minor modifications to command line tool so that it is easier to extend it to use with different solvers

--- tools/tulip_synth.py
+++ tools/tulip_synth.py
@@ -1,52 +1,69 @@
 #!/usr/bin/env python
-""" A script to use TuLiP from commandline
+""" A script to use TuLiP (with jtlv) from command line
+(other solvers not supported yet)
 
 Input: .yaml file that containts a problem description
 Output: .xml file that contains the problem description and synthesized automatton
 
-Command line usage: python tulip_synth.py your_yaml_file.yaml
+Command line usage: python tulip_synth.py your_yaml_file.yaml [output_file.xml]
 
 Necmiye Ozay (necmiye@cds.caltech.edu)
-April 19, 2013
+April 19, 2013. 
+Minor edits Apr 21, 2013.
 """
 
-#@importvardyn@
 import sys, os
 from numpy import array
 
 from tulip import *
-from tulip import parsespec
 from tulip import conxml
 
-testfile = 'temp'
-path = os.path.abspath(os.path.dirname(sys.argv[0]))
-smvfile = os.path.join(testfile+'.smv')
-spcfile = os.path.join(testfile+'.spc')
-autfile = os.path.join(testfile+'.aut')
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print "Usage: %s your_yaml_file.yaml [output_file.xml]" % sys.argv[0]
+        exit(1)
+        
+    inputfile = sys.argv[1]
 
-inputfile = sys.argv[1]
+    (sys_dyn, initial_partition, N, assumption, guarantee, env_vars, sys_disc_vars) = \
+                        conxml.readYAMLfile(inputfile)
 
-(sys_dyn, initial_partition, N, assumption, guarantee, env_vars, sys_disc_vars) = conxml.readYAMLfile(inputfile)
-
-disc_dynamics = discretize.discretize(initial_partition, sys_dyn, closed_loop=True, \
-                    N=N, min_cell_volume=0.1, verbose=0)
+    disc_dynamics = discretize.discretize(initial_partition, sys_dyn, closed_loop=True, \
+                        N=N, min_cell_volume=0.1, verbose=0)
+                        
+    ### jtlv specific part (should be within an if-then statement when solver is optional) ###
+    testfile = 'temp'
+    path = os.path.abspath(os.path.dirname(sys.argv[0]))
+    smvfile = os.path.join(testfile+'.smv')
+    spcfile = os.path.join(testfile+'.spc')
+    autfile = os.path.join(testfile+'.aut')
                     
-prob = jtlvint.generateJTLVInput(env_vars, sys_disc_vars, [assumption, guarantee],
-                    {}, disc_dynamics, smvfile, spcfile, verbose=0)
+    prob = jtlvint.generateJTLVInput(env_vars, sys_disc_vars, [assumption, guarantee],
+                        {}, disc_dynamics, smvfile, spcfile, verbose=0)
                     
-jtlvint.computeStrategy(smv_file=smvfile, spc_file=spcfile, aut_file=autfile,
-                    priority_kind=3, verbose=0)
+    jtlvint.computeStrategy(smv_file=smvfile, spc_file=spcfile, aut_file=autfile,
+                        priority_kind=3, verbose=0)
+    
                     
-aut = automaton.Automaton(autfile, [], 0)
+    aut = automaton.Automaton(autfile, [], 0)
+    
+    # Remove temporary files
+    os.remove(smvfile)
+    os.remove(spcfile)
+    os.remove(autfile)
+    
+    ### end of jtlv specific part ###
 
-# Remove dead-end states from automaton
-aut.trimDeadStates()
+    # Remove dead-end states from automaton
+    aut.trimDeadStates()
+    
+    # If the output filename is not specified use the input filename for output
+    if len(sys.argv) < 3:
+        outputfile = inputfile[:-5]+'.xml'
+    else:
+        outputfile = sys.argv[2]
 
-conxml.writeXMLfile("rsimple_example.xml", prob, [assumption, guarantee], sys_dyn, aut, pretty=True)
-
-os.remove(smvfile)
-os.remove(spcfile)
-os.remove(autfile)
+    conxml.writeXMLfile(outputfile, prob, [assumption, guarantee], sys_dyn, aut, pretty=True)
 
 
 
