f974e8310604f7523a26017cc955acbe3a8348f6
Necmiye Ozay <necmiye.ozay@gmail.com>
2013-06-30 11:47:11 -0700

    Placing new proposition preserving partition class under tulip. Changes: (i) more efficient implementation of prop2part, (ii) sparse adjacency matrix, (iii) PropPreservingPartition objects no more include transitions and will not be used as the output of abstraction as the latter will be represented by transition systems.

--- tulip/prop2part.py
+++ tulip/prop2part.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011, 2012 by California Institute of Technology
+# Copyright (c) 2011, 2012, 2013 by California Institute of Technology
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,18 +30,19 @@
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 # 
-# $Id$
+
 """ 
 Proposition preserving partition module.
 """
 
 
 import numpy as np
+from scipy import sparse as sp
 import copy
 
 import polytope as pc
 
-def prop2part2(state_space, cont_props_dict):
+def prop2part(state_space, cont_props_dict):
     """Main function that takes a domain (state_space) and a list of
     propositions (cont_props), and returns a proposition preserving
     partition of the state space.
@@ -59,53 +60,38 @@ def prop2part2(state_space, cont_props_dict):
     for prop_count in range(num_props):
         num_reg = mypartition.num_regions
         prop_holds_reg = []
-        prop_holds_poly = []
-        prop_not_holds_poly = []
         for i in range(num_reg): #i region counter
-            region_now = mypartition.list_region[i].list_poly[:]
+            region_now = mypartition.list_region[i].copy()
             #loop for prop holds
             prop_holds_reg.append(0)
-            prop_holds_poly[:] = []
             list_prop_now = mypartition.list_region[i].list_prop[:]
-            for j in range(len(region_now)): #j polytope counter
-                prop_holds_poly.append(0)
-                dummy = pc.Polytope(np.concatenate((region_now[j].A, cont_props[prop_count].A)),
-                                    np.concatenate((region_now[j].b, cont_props[prop_count].b)))
-                if pc.is_fulldim(dummy):
-                    #dummy = pc.reduce(dummy)
-                    mypartition.list_region[i].list_poly[j] = pc.reduce(dummy)
-                    prop_holds_reg[-1] = 1
-                    prop_holds_poly[-1] = 1
-            count = 0
-            for hold_count in range(len(prop_holds_poly)):
-                if prop_holds_poly[hold_count]==0:
-                    mypartition.list_region[i].list_poly.pop(hold_count-count)
-                    count+=1
-            if len(mypartition.list_region[i].list_poly)>0:
-                mypartition.list_region[i].list_prop.append(1)
+            dummy = pc.intersect(region_now, cont_props[prop_count])
+            if pc.is_fulldim(dummy):
+                dum_list_prop = list_prop_now[:]
+                dum_list_prop.append(1)
+                if len(dummy) == 0:
+                    mypartition.list_region[i] = pc.Region([dummy],dum_list_prop)
+                else:
+                    dummy.list_prop = dum_list_prop
+                    mypartition.list_region[i] = dummy.copy()
+                prop_holds_reg[-1] = 1
+            else: #does not hold in the whole region (-> no need for the 2nd loop)
+                region_now.list_prop.append(0)
+                mypartition.list_region.append(region_now)
+                continue
+                
             #loop for prop does not hold
             mypartition.list_region.append(pc.Region(list_poly=[],
                                                      list_prop=list_prop_now))
-            for j in range(len(region_now)):
-                valid_props = cont_props[prop_count] #eliminateRedundantProps(region_now[j],cont_props[prop_count])
-                A_prop = valid_props.A.copy()
-                b_prop = valid_props.b.copy()
-                num_halfspace = A_prop.shape[0]
-                for k in range(pow(2,num_halfspace)-1):
-                    signs = pc.num_bin(k,places=num_halfspace)
-                    A_now = A_prop.copy()
-                    b_now = b_prop.copy()
-                    for l in range(len(signs)): 
-                        if signs[l]==0:
-                            A_now[l] = -A_now[l]
-                            b_now[l] = -b_now[l]
-                    dummy = pc.Polytope(np.concatenate((region_now[j].A, A_now)),
-                                        np.concatenate((region_now[j].b, b_now)))
-                    if pc.is_fulldim(dummy):
-                        #dummy = pc.reduce(dummy)
-                        mypartition.list_region[-1].list_poly.append(pc.reduce(dummy))
-            if len(mypartition.list_region[-1].list_poly)>0:
-                mypartition.list_region[-1].list_prop.append(0)
+            dummy = pc.mldivide(region_now, cont_props[prop_count])
+            if pc.is_fulldim(dummy):
+                dum_list_prop = list_prop_now[:]
+                dum_list_prop.append(0)
+                if len(dummy) == 0:
+                    mypartition.list_region[-1] = pc.Region([pc.reduce(dummy)],dum_list_prop)
+                else:
+                    dummy.list_prop = dum_list_prop
+                    mypartition.list_region[-1] = dummy.copy()
             else:
                 mypartition.list_region.pop()
         count = 0
@@ -115,12 +101,12 @@ def prop2part2(state_space, cont_props_dict):
                 count+=1
         num_reg = len(mypartition.list_region)
         mypartition.num_regions = num_reg
-    adj = np.zeros((num_reg,num_reg), dtype=np.int8)
+    adj = sp.csr_matrix((num_reg,num_reg), dtype=np.int8)
     for i in range(num_reg):
         for j in range(i+1,num_reg):
             adj[i,j] = pc.is_adjacent(mypartition.list_region[i],
                                            mypartition.list_region[j])
-    adj =  adj+adj.T+np.eye(num_reg, dtype=np.int8)
+    adj =  adj+adj.T+sp.eye(num_reg, num_reg, dtype=np.int8)
     mypartition.adj = adj.copy()
     return mypartition
     
@@ -137,12 +123,12 @@ def prop2partconvex(ppp):
             myconvexpartition.list_region.append(region_now) 
     num_reg = len(myconvexpartition.list_region)
     myconvexpartition.num_regions = num_reg
-    adj = np.zeros((num_reg,num_reg), dtype=np.int8)
+    adj = sp.csr_matrix((num_reg,num_reg), dtype=np.int8)
     for i in range(num_reg):
         for j in range(i+1,num_reg):
             adj[i,j] = pc.is_adjacent(myconvexpartition.list_region[i],
                                            myconvexpartition.list_region[j])
-    adj =  adj+adj.T+np.eye(num_reg, dtype=np.int8)
+    adj =  adj+adj.T+sp.eye(num_reg, num_reg, dtype=np.int8)
     myconvexpartition.adj = adj.copy()
     return myconvexpartition
     
@@ -186,7 +172,7 @@ def pwa_partition(pwa_sys, ppp, abs_tol=1e-5):
                 new_list.append(isect)
                 parent.append(j)
     
-    adj = np.zeros([len(new_list), len(new_list)], dtype=int)
+    adj = sp.csr_matrix((len(new_list), len(new_list)), dtype=np.int8)
     for i in range(len(new_list)):
         for j in range(i+1, len(new_list)):
             if (ppp.adj[parent[i], parent[j]] == 1) or \
@@ -198,9 +184,8 @@ def pwa_partition(pwa_sys, ppp, abs_tol=1e-5):
             
     return PropPreservingPartition(domain=ppp.domain,\
                     num_prop=ppp.num_prop, list_region=new_list, num_regions=len(new_list), \
-                    adj=adj, trans=None, list_prop_symbol=ppp.list_prop_symbol, 
-                    list_subsys = subsys_list)
-
+                    adj=adj, list_prop_symbol=ppp.list_prop_symbol, list_subsys = subsys_list)        
+        
 
 class PropPreservingPartition:
     """Partition class with following fields
@@ -209,27 +194,19 @@ class PropPreservingPartition:
     - num_prop: number of propositions
     - list_region: proposition preserving regions, type: list of Region
     - num_regions: length of the above list
-    - adj: a matrix showing which regions are adjacent
-    - trans: a matrix showing which region is reachable from which region.
-             trans[i,j] = 1 means state i is reachable from state j.
+    - adj: a sparse matrix showing which regions are adjacent
     - list_prop_symbol: list of symbols of propositions
-    - orig_list_region: original proposition preserving regions
-    - orig: list assigning an original proposition preserving region to each
-            new region
-    - list_subsys: list assigning the subsystem of the picewise affine system that 
+    - list_subsys: list assigning the subsystem of the piecewise affine system that 
               is active in that region to each region in ppp 
 
     """
-    def __init__(self, domain=None, num_prop=0, list_region=[], num_regions=0, adj=0, trans=0, list_prop_symbol=None, orig_list_region=None, orig=None, list_subsys=None):
+    def __init__(self, domain=None, num_prop=0, list_region=[], num_regions=0, adj=0, list_prop_symbol=None, list_subsys=None):
         self.domain = domain
         self.num_prop = num_prop
         self.list_region = list_region[:]
         self.num_regions = len(list_region)
         self.adj = adj
-        self.trans = trans
         self.list_prop_symbol = list_prop_symbol
-        self.orig_list_region = orig_list_region
-        self.orig = orig
         self.list_subsys = list_subsys
         
     def reg2props(self, region):
@@ -242,8 +219,6 @@ class PropPreservingPartition:
             for j in range(len(self.list_region)):
                 output += "Region "+str(j)+", propositions: "+" ".join([self.list_prop_symbol[i] for i in range(len(self.list_prop_symbol)) if self.list_region[j].list_prop[i] != 0])+"\n"
                 output += str(self.list_region[j])
-        if hasattr(self.trans, "shape"):  # e.g., NumPy ndarrays have a shape
-            output +="Transition matrix:\n"+str(self.trans)+"\n"
-        elif hasattr(self.adj, "shape"):
-            output +="Adjacency matrix:\n"+str(self.adj)+"\n"
+        if hasattr(self.adj, "shape"):
+            output +="Adjacency matrix:\n"+str(self.adj.todense())+"\n"
         return output
