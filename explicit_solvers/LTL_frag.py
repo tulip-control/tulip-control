7f77e79f7fdb123449e1bfaccbd12ec61874c4f6
Eric Wolff <ewolff@caltech.edu>
2013-07-22 03:14:07 -0700

    Sketch of algorithms that will eventually be used for computing control policies for GR1 and Eric's fragment of LTL using an explicit transition system (non-symbolic).

---
+++ explicit_solvers/LTL_frag.py
@@ -0,0 +1,141 @@
+'''
+Created on Aug 13, 2012
+
+@author: ewolff
+'''
+import numpy as np
+import time
+import ex_graph as gr
+import random
+import itertools
+import networkx as nx
+
+######################################################################
+## Start of main function
+######################################################################
+ti = time.time()
+
+## INPUT:  TS, spec, prop2states, costs
+
+
+
+TS_orig = TS.copy()
+print spec
+
+print "Original TS ",len(TS)#, TS
+
+## Remove all actions that don't satisfy [](p --> X q) specs.
+t0 = time.time()
+TS = gr.remove_unsafe_actions(TS,spec['G_act'],prop2states)
+t1 = time.time()
+print "Safe actions",len(TS)#, TS
+print t1-t0,"(sec)"
+
+## Determine all nodes that are blocking, i.e., don't have any valid actions left
+t0 = time.time()
+blockingNodes = gr.blocking_nodes(TS)
+t1 = time.time()
+print "blockingNodes:",len(blockingNodes)
+print t1-t0,"(sec)"
+
+
+## Remove all states that don't satisfy safety [] specs (or must visit a state that doesn't)
+if spec['G'] != '':
+    t0 = time.time()
+    unsafeNodes = gr.must_reach_target(TS, set(TS.states.values()) - prop2states[spec['G']])
+    t1 = time.time()
+    print "must_reachTarget",t1-t0,"(sec)"
+    print "unsafeNodes:",len(unsafeNodes)
+    unsafeNodes.update(blockingNodes)   # add the blocking nodes to those that need to be removed
+    t0 = time.time()
+    TS = gr.subgraph(TS,unsafeNodes)
+    t1 = time.time()
+    print "subgraph:",t1-t0,"(sec)"
+    print "Safe state",len(TS)#, TS
+    
+## At this point, I have incorporated the [] and [](a -- b) safety properties.  Check to see if subgraph is non-empty.
+if len(TS) == 0:
+    print "The specification is invalid.  The [] and/or [](p --> X q) specs are too constraining."
+    quit()
+
+
+## Further restrict to the subgraph where the <>[] persistence property holds
+TS_per = TS.copy()
+if spec['FG'] != '':
+    TS_per = gr.subgraph(TS_per, set(TS.states.values()) - prop2states[spec['FG']])
+    print "Persistence",len(TS_per)#, TS_per
+
+
+if spec['GF'] != '':
+    ## Create paths between liveness states on TS_per subgraph
+    reachF = dict()     #forced reach sets for accepting state sets F
+    F = dict()          #sets of accepting states
+    
+    # Determine the states associated with each acceptance condition
+    taskInd = range(len(spec['GF']))
+    for i in taskInd:
+        f = spec['GF'][i]
+        F[i] = prop2states[f].copy()
+        assert len(F[i]) != 0
+        assert isinstance(F[i],set)
+    
+    # Compute generalized Buchi winning sets
+    while True:
+        print "working..."
+        for i in taskInd[:-1]:
+            t0 = time.time()
+            if costs is None:
+                reachF[i] = gr.reach_value(TS_per, F[i]) 
+            else:
+                reachF[i] = gr.opt_reach_value(TS_per, F[i], costs)
+            t1 = time.time()
+            print "reachF[",i,"] computed in",t1-t0,"sec."
+            F[i+1] = F[i+1] & set(reachF[i].keys())
+            assert isinstance(F[i+1],set)
+            if len(F[i+1]) == 0:
+                print "The specification is invalid. The <>[] and/or []<> specs are too constraining."
+                quit()
+
+        # Compute reachValue for last task
+        first = taskInd[0]
+        last = taskInd[-1]
+        t0 = time.time()
+        if costs is None:
+            reachF[last] = gr.reach_value(TS_per, F[last])
+        else:
+            reachF[last] = gr.opt_reach_value(TS_per, F[i], costs)
+        t1 = time.time()
+        print "reachF[",last,"] computed in",t1-t0,"sec."
+        
+        F_new = F[first] & set(reachF[last].keys())
+        
+        if F[first] == F_new:
+            break   # We have found a feasible control policy
+        F[first] = F_new
+        
+    # Compute the task graph between generalized winning sets F[j] from above
+    F_dict = dict()
+    for i in taskInd:
+        F_dict[i] = []
+        F_dict[i].append(F[i])
+        
+    G_task, node2states, node2task, task2node = gr.task_graph(TS_per, F_dict, costs)
+    print task2node
+    
+#    # Determine an optimal ordering and subsets of task sets F*[j]
+#     1. Average cost per task cycle.  Here, I output the task graph and attempt to solve 
+#     it using a TSP solver.  Branch and bound techniques work nicely here.
+#    tcVal, tcOrder, tcNodeOrder = opt.task_cycle(G_task,taskInd,task2node,weight='weight')
+#    print "Task cycle optimal value:", tcVal,", order:",tcOrder,", and node order:",tcNodeOrder
+
+#    # 2. Bottleneck cost between tasks
+#    botVal,botStates = opt.bottleneck(G_task,taskInd,task2node)
+#    print "Bottleneck optimal value:", botVal,"and states:",botStates
+
+else:
+    print "TODO"
+
+
+tf = time.time()
+print "Total time (sec) =",tf-ti
+print "NEED TO UPDATE THE PARENTS DIRECTORY WHEN DELETING ACTIONS WHILE CREATING SUBGRAPHS AND SUCH."
