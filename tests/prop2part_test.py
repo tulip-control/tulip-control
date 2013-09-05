c55378987be5035e837bda129f80bde79ecdc0d6
Necmiye Ozay <necmiye.ozay@gmail.com>
2013-09-04 22:27:23 -0400

    changed the partition in prop2part_test with a partition more suitable for numerical computation (i.e., no regions touching through a corner. now the test works both with glpk and cvxopt native solver.

--- tests/prop2part_test.py
+++ tests/prop2part_test.py
@@ -15,40 +15,27 @@ def prop2part_test():
                        [-1., 0.],
                        [0., 1.],
                        [0., -1.]]))
-    b.append(np.array([[1., 0., 1., 0.]]).T)
+    b.append(np.array([[.5, 0., .5, 0.]]).T)
     cont_props.append(pc.Polytope(A[0], b[0]))
     
     A.append(np.array([[1., 0.],
                        [-1., 0.],
                        [0., 1.],
                        [0., -1.]]))
-    b.append(np.array([[1., 0., 2., -1.]]).T)
+    b.append(np.array([[2., -1.5, 2., -1.5]]).T)
     cont_props.append(pc.Polytope(A[1], b[1]))
     
-    A.append(np.array([[1., 0.],
-                       [-1., 0.],
-                       [0., 1.],
-                       [0., -1.]]))
-    b.append(np.array([[2., -1., 1., 0.]]).T)
-    cont_props.append(pc.Polytope(A[2], b[2]))
-
-    A.append(np.array([[1., 0.],
-                       [-1., 0.],
-                       [0., 1.],
-                       [0., -1.]]))
-    b.append(np.array([[2., -1., 2., -1.]]).T)
-    cont_props.append(pc.Polytope(A[3], b[3]))
-    
-    cont_props_dict = dict([("C"+str(i), pc.Polytope(A[i], b[i])) for i in range(4)])
+    cont_props_dict = dict([("C"+str(i), pc.Polytope(A[i], b[i])) for i in range(2)])
     
     
     mypartition = prop2part.prop2part(state_space, cont_props_dict)
-    ref_adjacency = np.array([[1,1,1,0],[1,1,0,1],[1,0,1,1],[0,1,1,1]])
+    ref_adjacency = np.array([[1,0,1],[0,1,1],[1,1,1]])
     assert np.all(mypartition.adj.todense() == ref_adjacency)
 
-    assert len(mypartition.list_region) == 4
-    for reg in mypartition.list_region:
-        assert len(reg.list_prop) == 4
+    assert len(mypartition.list_region) == 3
+    
+    for reg in mypartition.list_region[0:2]:
+        assert len(reg.list_prop) == 2
         assert len(reg.list_poly) == 1
         i = [i for i in range(len(reg.list_prop)) if reg.list_prop[i] == 1]
         assert len(i) == 1
@@ -59,4 +46,13 @@ def prop2part_test():
         actual_V = pc.extreme(reg.list_poly[0])
         actual_V = set([(v[0],v[1]) for v in actual_V.tolist()])
         assert ref_V == actual_V
+        
+    assert len(mypartition.list_region[2].list_prop) == 2
+    assert sum(mypartition.list_region[2].list_prop) == 0
+    assert len(mypartition.list_region[2].list_poly) == 3
+    dum = state_space.copy()
+    for reg in mypartition.list_region[0:2]:
+        dum = pc.mldivide(dum,reg)
+    assert pc.is_empty(pc.mldivide(dum,mypartition.list_region[2]))
+    assert pc.is_empty(pc.mldivide(mypartition.list_region[2],dum))
 
