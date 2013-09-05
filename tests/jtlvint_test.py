e6e72cdad7db8412b192447a124dcc1ad4076dff
Scott Livingston <slivingston@cds.caltech.edu>
2013-09-05 16:02:32 -0700

    TEST: Better (regression-like) test of jtlvint.synthesize

--- tests/jtlvint_test.py
+++ tests/jtlvint_test.py
@@ -37,4 +37,25 @@ def test_synthesize():
                   sys_init="y",
                   sys_prog=["y & x", "!y"])
     aut = synthesize(spec)
+    # There is more than one possible strategy realizing this
+    # specification.  Checking only for one here makes this more like
+    # a regression test (fragile).  However, it is more meaningful
+    # than simply checking that synthesize() returns something
+    # non-None (i.e., realizability, which is tested elsewhere).
     assert aut is not None
+    assert len(aut.inputs) == 1 and aut.inputs.has_key("x")
+    assert len(aut.outputs) == 1 and aut.outputs.has_key("y")
+    assert set(aut.states()) == set(range(5))
+    assert set(aut.transitions()) == set([(0, 1), (0, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 0), (3, 3), (4, 0), (4, 3)])
+    label_reference = {(0, 1) : (0,0),  # value is bitvector of x,y
+                       (0, 2) : (1,0),
+                       (1, 3) : (0,0),
+                       (1, 4) : (1,0),
+                       (2, 3) : (0,0),
+                       (2, 4) : (1,0),
+                       (3, 0) : (1,1),
+                       (3, 3) : (0,0),
+                       (4, 0) : (1,1),
+                       (4, 3) : (0,0)}
+    for (from_state, to_state, slabel) in aut.transitions(labeled=True):
+        assert label_reference[(from_state, to_state)] == (slabel["x"], slabel["y"])
