dafa419eddd124a8ab7c021dcdbbef6ab0cecff7
Ioannis Filippidis <jfilippidis@gmail.com>
2014-01-26 17:58:23 -0800

    extract pyparsing usage from spec.ast to spec.parse

---
+++ tulip/spec/parse.py
@@ -0,0 +1,153 @@
+# Copyright (c) 2011-2014 by California Institute of Technology
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the California Institute of Technology nor
+#    the names of its contributors may be used to endorse or promote
+#    products derived from this software without specific prior
+#    written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CALTECH
+# OR THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+# 
+"""
+LTL parser supporting JTLV, SPIN, SMV, and gr1c syntax
+
+Syntax taken originally roughly from http://spot.lip6.fr/wiki/LtlSyntax
+"""
+from pyparsing import *
+import sys
+
+from .ast import LTLException, ASTVar, ASTNum, ASTBool, ASTArithmetic, \
+    ASTComparator, ASTUnTempOp, ASTBiTempOp, ASTUnary, ASTBinary, \
+    ASTNot, ASTAnd, ASTOr, ASTXor, ASTImp, ASTBiImp
+
+# Packrat parsing - it's much faster
+ParserElement.enablePackrat()
+
+# Literals cannot start with G, F or X unless quoted
+_restricted_alphas = filter(lambda x: x not in "GFX", alphas)
+
+# Quirk: allow literals of the form:
+#   (G|F|X)[0-9_][A-Za-z0-9._]*
+# so we can have X0 etc.
+_bool_keyword = CaselessKeyword("TRUE") | CaselessKeyword("FALSE")
+
+_var = ~_bool_keyword + (
+    Word(_restricted_alphas, alphanums + "._:") | \
+    Regex("[A-Za-z][0-9_][A-Za-z0-9._:]*") | QuotedString('"')
+).setParseAction(ASTVar)
+
+_atom = _var | _bool_keyword.setParseAction(ASTBool)
+_number = _var | Word(nums).setParseAction(ASTNum)
+
+# arithmetic expression
+_arith_expr = operatorPrecedence(
+    _number,
+    [
+        (oneOf("* /"), 2, opAssoc.LEFT, ASTArithmetic),
+        (oneOf("+ -"), 2, opAssoc.LEFT, ASTArithmetic),
+        ("mod", 2, opAssoc.LEFT, ASTArithmetic)
+    ]
+)
+
+# integer comparison expression
+_comparison_expr = Group(
+    _arith_expr + oneOf("< <= > >= != = ==") + _arith_expr
+).setParseAction(ASTComparator)
+
+_proposition = _comparison_expr | _atom
+
+# hack so G/F/X doesn't mess with keywords
+#(i.e. FALSE) or variables like X0, X_0_1
+_UnaryTempOps = ~_bool_keyword + \
+    oneOf("G F X [] <> next") + ~Word(nums + "_")
+
+def extract_vars(tree):
+    v = []
+    def f(t):
+        if isinstance(t, ASTVar):
+            v.append(t.val)
+        return t
+    tree.map(f)
+    return v
+    
+# Crude test for safety spec
+def issafety(tree):
+    def f(t):
+        if isinstance(t, ASTUnTempOp) and not t.operator == "G":
+            return False
+        if isinstance(t, ASTBiTempOp):
+            return False
+        if isinstance(t, ASTUnary):
+            return t.operand
+        if isinstance(t, ASTBinary):
+            return (t.op_l and t.op_r)
+        return True
+    return tree.map(f)
+
+def parse(formula):
+    """Parse formula string and create abstract syntax tree (AST).
+    """
+    # LTL expression
+    _ltl_expr = operatorPrecedence(
+        _proposition,
+       [("'", 1, opAssoc.LEFT, ASTUnTempOp),
+        ("!", 1, opAssoc.RIGHT, ASTNot),
+        (_UnaryTempOps, 1, opAssoc.RIGHT, ASTUnTempOp),
+        (oneOf("& &&"), 2, opAssoc.LEFT, ASTAnd),
+        (oneOf("| ||"), 2, opAssoc.LEFT, ASTOr),
+        (oneOf("xor ^"), 2, opAssoc.LEFT, ASTXor),
+        ("->", 2, opAssoc.RIGHT, ASTImp),
+        ("<->", 2, opAssoc.RIGHT, ASTBiImp),
+        (oneOf("= == !="), 2, opAssoc.RIGHT, ASTComparator),
+        (oneOf("U V R"), 2, opAssoc.RIGHT, ASTBiTempOp)]
+    )
+    _ltl_expr.ignore(LineStart() + "--" + restOfLine)
+
+    # Increase recursion limit for complex formulae
+    sys.setrecursionlimit(2000)
+    try:
+        return _ltl_expr.parseString(formula, parseAll=True)[0]
+    except RuntimeError:
+        raise ParseException(
+            "Maximum recursion depth exceeded,"
+            "could not parse"
+        )
+
+if __name__ == "__main__":
+    try:
+        ast = parse(sys.argv[1])
+    except ParseException as e:
+        print("Parse error: " + str(e) )
+        sys.exit(1)
+    print("Parsed expression: " + str(ast) )
+    print("Length: " +str( len(ast) ) )
+    print("Variables: " + str(extract_vars(ast) ) )
+    print("Safety: " +str(issafety(ast) ) )
+    try:
+        print("JTLV syntax: " +str(ast.to_jtlv() ) )
+        print("SMV syntax: " +str(ast.to_smv() ) )
+        print("Promela syntax: " +str(ast.to_promela() ) )
+    except LTLException as e:
+        print(e.message)
