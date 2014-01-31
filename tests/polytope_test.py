ac790a01449919dc7ea768b972d81ebd39532fbc
Ioannis Filippidis <jfilippidis@gmail.com>
2014-01-31 13:38:10 -0800

    convert raw test code to functions
    for correct use of nose

--- tests/polytope_test.py
+++ tests/polytope_test.py
@@ -14,16 +14,17 @@ Ab = np.array([[0.0, 1.0, 1.0],
 A = Ab[:,0:2]
 b = Ab[:, 2]
 
-p = pc.Polytope(A, b)
-p2 = pc.Polytope(A, 2*b)
-
-assert(p <= p2)
-assert(not p2 <= p)
-assert(not p2 == p)
-
-r = pc.Region([p])
-r2 = pc.Region([p2])
-
-assert(r <= r2)
-assert(not r2 <= r)
-assert(not r2 == r)
+def comparison_test():
+    p = pc.Polytope(A, b)
+    p2 = pc.Polytope(A, 2*b)
+    
+    assert(p <= p2)
+    assert(not p2 <= p)
+    assert(not p2 == p)
+    
+    r = pc.Region([p])
+    r2 = pc.Region([p2])
+    
+    assert(r <= r2)
+    assert(not r2 <= r)
+    assert(not r2 == r)
