f703c66fa144df83c79bac03b32641ad0f430e15
Ioannis Filippidis <jfilippidis@gmail.com>
2014-03-02 15:59:10 -0800

    untested draft for unix installation

--- contrib/nessainstall/install.sh
+++ contrib/nessainstall/install.sh
@@ -1,155 +1,140 @@
-Installs Tulip on Nessa. Hopefully, this is still easier than installing Tulip
-on Windows.
+# install script for unix-like systems
+# assumes git is installed on you machine
 
+# edit these to your preference
 
+# e.g.: ~/.bash_profile if using bash,
+# or:   ~/.tcshrc if using csh
+export CFG_FILE=./test.txt
 
-1) Create libraries folder and permanently add to path
+# where the `libraries` directory will go,
+# will contain python, ATLAS, LAPACK, glpk, gr1c
+INSTALL_LOC=~/
 
-	mkdir ~/libraries
-	mkdir ~/libraries/bin
-	mkdir ~/libraries/include
-	mkdir ~/libraries/lib
-	mkdir ~/libraries/share
+#------------------------------------------------------------
+# do not edit below unless you know what you are doing
+TMPLIB=$INSTALL_LOC/libraries
 
-   Add the lines below to ~/.tcshrc:
+# create libraries to install things
+mkdir ~/libraries
 
-   	export PATH /home/username/libraries/bin:$PATH
-	export LD_LIBRARY_PATH /home/username/libraries/lib
+# in bash use export,
+# in csh use setenv
+sed '$ a\
+	export PATH="$TMPLIB"/bin:$PATH' $CFG_FILE
+sed '$ a\
+	export LD_LIBRARY_PATH="$TMPLIB"/lib' $CFG_FILE
 
-   Reload .tcshrc:
+source $CFG_FILE
+cd $INSTALL_LOC
 
-   	source ~/.tcshrc
+#------------------------------------------------------------
+# install ATLAS with LAPACK
+wget http://sourceforge.net/projects/math-atlas/files/Stable/3.10.1/atlas3.10.1.tar.bz2
+wget http://www.netlib.org/lapack/lapack-3.5.0.tgz
 
+tar xjf atlas3.10.1.tar.bz2 # unpack only ATLAS
+cd ATLAS
+mkdir LinuxBuild
+cd LinuxBuild
+../configure -b 64 --prefix=$TMPLIB --shared \
+	--with-netlib-lapack-tarfile=../lapack-3.5.0.tgz
+make build # this takes forever...
+make check
+make ptcheck # should be no errors for make check, ptcheck, time
+make time
+make install
 
+#------------------------------------------------------------
+# install python
+wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tgz
+tar xzf Python-2.7.6.tgz
+cd Python-2.7.6
+./configure --prefix=$TMPLIB --enable-shared
+make
+make install
 
-2) Install ATLAS with LAPACK
+#------------------------------------------------------------
+# install pip
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
+source $CFG_FILE
 
-	a. Download ATLAS 3.10.1: http://sourceforge.net/projects/math-atlas
+#------------------------------------------------------------
+# install python packages
+pip install numpy
+pip install scipy
+pip install matplotlib
+pip install ply
+pip install virtualenvwrapper # optional for your convenience
 
-	b. Download LAPACK 3.5: http://www.netlib.org/lapack/#_lapack_version_3_5_0
+# downgrade pyparsing
+pip uninstall pyparsing
+pip install -Iv https://pypi.python.org/packages/source/p/pyparsing/pyparsing-1.5.7.tar.gz#md5=9be0fcdcc595199c646ab317c1d9a709
 
-	c. Untar the ATLAS archive, but not the LAPACK archive:
+# install latest pydot version
+pip install http://pydot.googlecode.com/files/pydot-1.0.28.tar.gz
 
-			tar xjf atlas3.10.1.tar.bz2
+pip install networkx
 
-	d. Enter the directory, compile, check, and install ATLAS
+#------------------------------------------------------------
+# install glpk
 
-			cd ATLAS
-			mkdir LinuxBuild
-			cd LinuxBuild
-			../configure -b 64 --prefix=/path/to/libraries --shared \
-				--with-netlib-lapack-tarfile=/path/to/lapack-3.5.0.tgz
-			make build      (this takes forever...)
-			make check
-			make ptcheck    (should be no errors for make check, ptcheck, time)
-			make time
-			make install
+# cvxopt is incompatible with newer versions
+wget http://ftp.gnu.org/gnu/glpk/glpk-4.48.tar.gz
+tar xzf glpk-4.48.tar.gz
+cd glpk-4.48
+./configure --prefix=$TMPLIB
+make
+make check # should return no errors
+make install
 
+#------------------------------------------------------------
+# install cvxopt
+wget http://abel.ee.ucla.edu/src/cvxopt-1.1.6.tar.gz
+tar xzf cvxopt-1.1.6.tar.gz
 
+# rename archive
+mv cvxopt-1.1.6.tar.gz cvxopt-orig.tar.gz
+cd cvxopt-1.1.6
 
-3) Install glpk
+# hack: edit setup.py
+sed '5 c\
+	BLAS_LIB_DIR = '"$TMPLIB"/lib' setup.py
+sed '8 c\
+	BLAS_LIB = [''satlas'', ''tatlas'', ''atlas'']' setup.py
+sed '9 c\
+	LAPACK_LIB = []' setup.py
+sed '10 c\
+	BLAS_EXTRA_LINK_ARGS = [''-nostdlib'']' setup.py
+sed '36 c\
+	BUILD_GLPK = 1' setup.py
+sed '39 c\
+	GLPK_LIB_DIR = ''"$TMPLIB"/lib''' setup.py
+sed '42 c\
+	GLPK_INC_DIR = ''"$TMPLIB"/include''' setup.py
 
-	a. Download glpk version 4.48 to your home directory. cvxopt is incompatible
-	   with newer versions
+# tar the edited package and install
+cd ..
+tar czf cvxopt-1.1.6.tar.gz cvxopt-1.1.6/
+pip install cvxopt-1.1.6.tar.gz
 
-	b. Untar glpk:
-		
-			tar xzf glpk-4.48.tar.gz
+#------------------------------------------------------------
+# install gr1c
 
-	c. Build and install glpk into your libraries directory:
+# download requires Caltech IP Address (change this to building it)
+wget http://vehicles.caltech.edu/private/snapshots/nessa/gr1c/gr1c-0.6.6-95ec9c6.tar.gz
 
-			cd glpk-4.48
-			./configure --prefix=/path/to/libraries
-			make
-			make check (should return no errors)
-			make install
+# untar and copy all binaries to your bin folder
+tar xzf gr1c-0.6.6-95ec9c6.tar.gz
+cd gr1c-0.6.6-95ec9c6
+cp gr1c rg grpatch $TMPLIB/bin 
 
+#------------------------------------------------------------
+# install tulip
+git clone https://github.com/tulip-control/tulip-control.git 
+cd tulip-control
+python setup.py install
 
-
-2) Download and install Python
-
-	wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tgz
-	tar xzf Python-2.7.6.tgz
-	cd Python-2.7.6
-	./configure --prefix=/home/stsuei/libraries --enable-shared
-	make
-	make install
-
-
-
-3) Install pip
-
-	wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-	python get-pip.py
-	source ~/.tcshrc
-
-
-
-4) Install numpy, scipy, matplotlib, ply
-
-	pip install numpy
-	pip install scipy
-	pip install matplotlib
-	pip install ply
-
-
-
-5) Downgrade pyparsing and install pydot, networkx
-
-	pip uninstall pyparsing
-	pip install -Iv https://pypi.python.org/packages/source/p/pyparsing/pyparsing-1.5.7.tar.gz#md5=9be0fcdcc595199c646ab317c1d9a709
-	easy_install pydot (don't use pip)
-	pip install networkx
-
-   See this page for more detail: http://stackoverflow.com/questions/15951748/pydot-and-graphviz-error-couldnt-import-dot-parser-loading-of-dot-files-will
-
-
-
-6) Install gr1c:
-
-	a. Download gr1c-0.6.6-95ec9c6.tar.gz from 
-	   http://vehicles.caltech.edu/private/snapshots/nessa/gr1c/
-	   (Requires Caltech IP Address)
-
-	b. Untar the package and copy all binaries to your bin folder:
-
-		tar xzf gr1c-0.6.6-95ec9c6.tar.gz
-		cd gr1c-0.6.6-95ec9c6
-		cp gr1c rg grpatch /path/to/libraries/bin 
-
-
-
-7) Install cvxopt:
-
-	a. Download cvxopt 1.1.6: http://abel.ee.ucla.edu/src/cvxopt-1.1.6.tar.gz
-	b. Untar and rename the archive:
-
-			tar xzf cvxopt-1.1.6.tar.gz
-			mv cvxopt-1.1.6.tar.gz cvxopt-orig.tar.gz
-			cd cvxopt-1.1.6
-
-	c. Edit setup file:
-
-	   		-Line 5:  BLAS_LIB_DIR = '/path/to/libraries/lib'
-			-Line 8:  BLAS_LIB = [ 'satlas', 'tatlas', 'atlas' ]
-			-Line 9:  LAPACK_LIB = []
-			-Line 10: BLAS_EXTRA_LINK_ARGS = [ '-nostdlib' ]
-			-Line 36: BUILD_GLPK = 1
-			-Line 39: GLPK_LIB_DIR = '/path/to/libraries/lib'
-			-Line 42: GLPK_INC_DIR = '/path/to/libraries/include'
-
-
-	  d. Tar the edited package and install with pip:
-
-	  		cd ..
-			tar czf cvxopt-1.1.6.tar.gz cvxopt-1.1.6/
-			pip install cvxopt-1.1.6.tar.gz
-
-
-
-8) Install tulip:
-
-	git clone https://github.com/tulip-control/tulip-control.git 
-	cd tulip-control
-	python setup.py build
-	python setup.py install
+# use this instead if you want to edit code in the git repository
+#python setup.py develop
