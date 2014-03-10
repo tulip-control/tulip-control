15a7a06cba426fece41db0e2c299a0acb472a096
Scott Livingston <slivingston@cds.caltech.edu>
2014-03-10 12:42:21 -0700

    TEST: Reorganize transys test files

--- tests/transys_mathset_test.py
+++ tests/transys_mathset_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-
 """
-Unit Tests for transys module.
+Tests for transys.mathset (part of transys subpackage)
 """
 
 from collections import Iterable
@@ -223,45 +222,3 @@ class PowerSet_operations_test:
         assert set([1,2,3]) in p
         assert self.singleton() == [(), (1,)]
         assert self.empty() == [()]
-
-
-def rabin_test():
-    dra = trs.DRA()
-    print(dra)
-    
-    dra.states.add_from(range(10) )
-    
-    dra.states.accepting.add([1,2], [3,4] )
-    assert(isinstance(dra.states.accepting._pairs, list) )
-    assert(dra.states.accepting._pairs[0][0]._set == {1,2} )
-    assert(dra.states.accepting._pairs[0][0]._list == [] )
-    assert(dra.states.accepting._pairs[0][1]._set == {3,4} )
-    assert(dra.states.accepting._pairs[0][1]._list == [] )
-    print(dra.states.accepting)
-    
-    dra.states.accepting.remove([1,2], [3,4] )
-    
-    assert(isinstance(dra.states.accepting._pairs, list) )
-    assert(not dra.states.accepting._pairs)
-    
-    dra.states.accepting.add([1], [2, 4] )
-    dra.states.accepting.add_states(0, [2], [] )
-    
-    dra.states.accepting.add([2, 1], [] )
-    
-    assert(isinstance(dra.states.accepting._pairs, list) )
-    assert(dra.states.accepting._pairs[0][0]._set == {1,2} )
-    assert(dra.states.accepting._pairs[0][0]._list == [] )
-    assert(dra.states.accepting._pairs[0][1]._set == {2,4} )
-    assert(dra.states.accepting._pairs[0][1]._list == [] )
-    
-    assert(dra.states.accepting._pairs[1][0]._set == {2,1} )
-    assert(dra.states.accepting._pairs[1][0]._list == [] )
-    assert(dra.states.accepting._pairs[1][1]._set == set() )
-    assert(dra.states.accepting._pairs[1][1]._list == [] )
-
-if __name__ == '__main__':
-    mathset_test()
-    subset_test()
-    powerset_test()
-    rabin_test()
