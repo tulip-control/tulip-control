118d33f23d2f00c3f1434fc70ac9a5be4fbaf9be
Scott Livingston <slivingston@cds.caltech.edu>
2014-03-10 12:37:24 -0700

    TEST: Begin LabeledStates, and ops for States

--- tests/transys_labeled_graphs_test.py
+++ tests/transys_labeled_graphs_test.py
@@ -6,6 +6,7 @@ Tests for transys.labeled_graphs (part of transys subpackage)
 from nose.tools import raises
 from networkx import MultiDiGraph
 from tulip.transys import labeled_graphs
+from tulip.transys.mathset import PowerSet, MathSet
 
 
 def str2singleton_test():
@@ -76,6 +77,17 @@ class States_test():
         self.S_mutable.remove([1, 2])
         assert set([s for s in self.S_mutable]) == set([-1])
 
+    def test_call(self):
+        self.S_immutable.add_from([-1, "Cal"])
+        S_imm_dat = self.S_immutable(data=True)
+        assert (len(S_imm_dat) == 2) and ((-1, dict()) in S_imm_dat) and \
+            (("Cal", dict()) in S_imm_dat)
+
+        self.S_mutable.add_from([[1,2], 3])
+        S_mut_dat = self.S_mutable(data=True)
+        assert (len(S_mut_dat) == 2) and (([1, 2], dict()) in S_mut_dat) and \
+            ((3, dict()) in S_mut_dat)
+
 
 class Transitions_test:
     def setUp(self):
@@ -93,6 +105,8 @@ class Transitions_test:
         assert len(self.T) == 1
         self.T.add(2, 3)
         assert len(self.T) == 2
+
+        # Transitions should be unaffected by new states
         self.T.graph.states.add_from([10])
         assert len(self.T) == 2
 
@@ -125,3 +139,74 @@ class Transitions_test:
     def test_between(self):
         self.T.add_from([5, 2], [4, 3], check_states=False)
         assert set(self.T.between([5, 2], [3,])) == set([(5, 3), (2, 3)])
+
+
+class LabeledStates_test:
+    def setUp(self):
+        G = labeled_graphs.LabeledStateDiGraph()
+        G._state_label_def = {"ap": PowerSet(MathSet(['p', 'q', 'r',
+                                                      'x', 'a', 'b']))}
+        self.S_immutable_ap = labeled_graphs.LabeledStates(G, mutable=False)
+        G.states = self.S_immutable_ap
+
+        G = labeled_graphs.LabeledStateDiGraph()
+        G._state_label_def = {"ap": PowerSet(MathSet(['p', 'q', 'r']))}
+        self.S_mutable_ap = labeled_graphs.LabeledStates(G, mutable=True)
+        G.states = self.S_mutable_ap
+
+    def tearDown(self):
+        self.S_immutable_ap = None
+        self.S_mutable_ap = None
+
+    @raises(Exception)
+    def test_label_missing_states(self):
+        self.S_immutable_ap.label(1, MathSet(['p']), check=True)
+
+    def test_label(self):
+        # This also tests label_of and labeled_with
+        self.S_immutable_ap.label(1, MathSet(['p']), check=False)
+        assert len(self.S_immutable_ap) == 1
+        self.S_immutable_ap.label(2, MathSet(['p']), check=False)
+        assert len(self.S_immutable_ap) == 2
+        self.S_immutable_ap.label(3, MathSet(['q', 'r']), check=False)
+        assert len(self.S_immutable_ap) == 3
+
+        assert self.S_immutable_ap.label_of(1) == {"ap": MathSet(['p'])}
+        assert self.S_immutable_ap.label_of(3) == {"ap": MathSet(['q', 'r'])}
+        assert set(self.S_immutable_ap.labeled_with({"ap": MathSet(['p'])})) == set([1, 2])
+
+    def test_labels(self):
+        self.S_immutable_ap.labels("create", [{'p'}, {'q'}])
+        assert len(self.S_immutable_ap) == 2
+        current_labels = [l["ap"] for (s,l) in self.S_immutable_ap(data=True)]
+        assert len(current_labels) == 2 and \
+            MathSet(current_labels) == MathSet([{'p'}, {'q'}])
+
+        self.S_immutable_ap.labels([(10, {'r'}), (11, {'x'})], check=False)
+        assert len(self.S_immutable_ap) == 4
+        self.S_immutable_ap.labels([(10, {'a'}), (11, {'b'})])
+        assert len(self.S_immutable_ap) == 4
+        current_labels = [l["ap"] for (s,l) in self.S_immutable_ap(data=True)]
+        assert len(current_labels) == 4 and \
+            MathSet(current_labels)== MathSet([{'p'}, {'q'}, {'a'}, {'b'}])
+
+        self.S_mutable_ap.labels(range(4), [{'p'}, {'q'}, {'a'}, {'b'}], check=False)
+        assert len(self.S_mutable_ap) == 4
+        assert MathSet([l["ap"] for (s,l) in self.S_immutable_ap(data=True)]) == MathSet(current_labels)
+
+    def test_find(self):
+        state_list = ["state"+str(i) for i in range(4)]
+        self.S_mutable_ap.labels(state_list, [{'p'}, {'q'}, {'p'}, {'q'}],
+                                 check=False)
+        result = self.S_mutable_ap.find("state1")
+        assert len(result) == 1 and result[0] == ("state1", {"ap": set(['q'])})
+
+        result = self.S_mutable_ap.find(["state1", "state0"])
+        assert len(result) == 2 and \
+            ("state1", {"ap": set(['q'])}) in result and \
+            ("state0", {"ap": set(['p'])}) in result
+
+        result = self.S_mutable_ap.find(desired_label={"ap": {'p'}})
+        print result
+        assert len(result) == 2 and \
+            set([s for (s,l) in result]) == set(["state0", "state2"])
