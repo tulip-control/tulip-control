9869f54188835aba8f888a686bcda57ced966055
Stephanie Tsuei <stephanietsuei@gmail.com>
2014-03-19 17:21:43 -0700

    Moving Stateflow generation script to MATLAB:
    1) tostateflow.py is gone.
    2) tomatlab now exports MealyMachines

--- contrib/matlab/tomatlab.py
+++ contrib/matlab/tomatlab.py
@@ -1,15 +1,13 @@
 """
 Only supports closed-loop and non-conservative simulation.
 """
-
 from tulip import hybrid, polytope, abstract
 import scipy.io
-import tosimulink
 import numpy
 
 
-def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
-    control_horizon=None, R=None, r=None, Q=None, mid_weight=None):
+def export(mealy_machine, system_dynamics=None, abstraction=None,
+    disc_params=None, R=None, r=None, Q=None, mid_weight=None):
 
     """Creates two matlab files. One is a script that generates a Simulink model
     that contains a Stateflow Chart of the Mealy Machine, a block that contains
@@ -23,16 +21,22 @@ def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
     """
 
     # Check whether we're discrete or continuous
-    if ((system_dynamics is None) and (abstraction is None)):
+    if ((system_dynamics is None) and (abstraction is None) and 
+        (disc_params is None)):
         is_continuous = False
-    elif ((system_dynamics is not None) and (abstraction is not None)):
+    elif ((system_dynamics is not None) and (abstraction is not None) and 
+          (disc_params is not None)):
         is_continuous = True
     else:
         raise StandardError('Cannot tell whether system is continuous or ' + 
-            'discrete. Please specify dynamics and abstraciton or neither.')
+            'discrete. Please specify dynamics and abstraciton and ' + 
+            'discretization parameters or none at all.')
 
+    output = {}
+    output['is_continuous'] = is_continuous
 
-    # Skip all but last part if the system is discrete.
+    # Only export dynamics and abstraction and control weights if the system is
+    # continuous
     if is_continuous:
 
         # Export system dynamics, get state and input dimension
@@ -52,11 +56,9 @@ def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
         else:
             raise TypeError(str(type(system)) + 
                 ' is not a supported type of system dynamics.')
+        output['system_dynamics'] = dynamics_output
 
-        # Set default values for matrix weights and other optional parameters
-        if control_horizon is None:
-            raise ValueError('Control horizon must be given for continuous ' +
-                'systems.')
+        # Control weights. Set default values if needed.
         if R is None:
             R = numpy.zeros([state_dimension, state_dimension])
         if r is None:
@@ -66,27 +68,42 @@ def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
         if mid_weight is None:
             mid_weight = 3
 
+        control_weights = {}
+        control_weights['state_weight'] = R
+        control_weights['input_weight'] = Q
+        control_weights['linear_weight'] = r
+        control_weights['mid_weight'] = mid_weight
+        output['control_weights'] = control_weights
 
-        # Miscellaneous simulation parameters that aren't in the dynamics
+        # Simulation parameters; insert default discretization values if needed
         sim_params = {}
-        sim_params['horizon'] = control_horizon
-        sim_params['state_weight'] = R
-        sim_params['input_weight'] = Q
-        sim_params['linear_weight'] = r
-        sim_params['mid_weight'] = mid_weight
-        
-        # Final data dictionary
-        output = {}
-        output['system_dynamics'] = dynamics_output
-        output['abstraction'] = export_locations(abstraction) #get abstraction
-        output['sim_params'] = sim_params
-        scipy.io.savemat(filename, output, oned_as='column')
+        try:
+            sim_params['horizon'] = disc_params['N']
+        except KeyError:
+            sim_params['horizon'] = 10
+        try:
+            sim_params['use_all_horizon'] = disc_params['use_all_horizon']
+        except KeyError:
+            sim_params['use_all_horizon'] = False
+        try:
+            sim_params['closed_loop'] = disc_params['closed_loop']
+        except KeyError:
+            sim_params['closed_loop'] = True
+        if 'conservative' in disc_params:
+            if disc_params['conservative'] is True:
+                raise ValueError('MATLAB interface does not suport ' + 
+                                 'conservative simulation')
+        output['simulation_parameters'] = sim_params
+
+        # Abstraction
+        output['abstraction'] = export_locations(abstraction)
 
 
     # Export the simulink model
-    tosimulink.export(mealy_machine, is_continuous, control_horizon, R, r, Q, 
-        mid_weight)
+    output['TS'] = export_mealy(mealy_machine, is_continuous)
 
+    # Save file
+    scipy.io.savemat('tulip_output.mat', output, oned_as='column')
 
 
 
@@ -182,5 +199,75 @@ def export_locations(abstraction):
         location_list.append(location_dict) 
 
     output['abstraction'] = location_list
+
+    return output
+
+
+def export_mealy(mealy_machine, is_continuous):
+    """Exports a Mealy Machine to data that can be put into a .mat file. Turns
+    the Mealy Machine states into a list of dictionaries. Turns the Mealy
+    Machine transitions into a list of transition matrices.
+
+    Some of the exported information is a bit redundant, but makes writing the
+    MATLAB code easier.
+
+    @rtype: dict
+    """
+
+    output = {}
+
+    # States will be exported as a list of dictionaries
+    state_list = []
+    for state_tuple in mealy_machine.states.find():
+
+        # Do not export Sinit state
+        if state_tuple[0] == 'Sinit':
+            continue
+
+        state_dict = {}
+        state_dict['name'] = state_tuple[0]
+
+        # For a continuous system, export the 'loc' variable
+        if is_continuous:
+            state_dict['loc'] = state_tuple[1]['loc']
+
+        state_list.append(state_dict)
+    output['states'] = state_list
+
+    # Get list of environment and system variables
+    env_vars = mealy_machine.inputs.keys()
+    sys_vars = mealy_machine.outputs.keys()
+    output['inputs'] = env_vars
+    output['outputs'] = sys_vars
+
+    # Transitions will be exported as a 2D list of dictionaries. The only
+    # purpose of this block here is to separate the inputs from the outputs to
+    # make the MATLAB code easier to write
+    transitions = []
+    for transition_tuple in mealy_machine.transitions.find():
+        transition_vals = transition_tuple[2]
+        transition_inputs = { var: transition_vals[var] for var in env_vars }
+        transition_outputs = { var: transition_vals[var] for var in sys_vars }
+        transition_dict = {}
+        transition_dict['start_state'] = transition_tuple[0]
+        transition_dict['end_state'] = transition_tuple[1]
+        transition_dict['inputs'] = transition_inputs
+        transition_dict['outputs'] = transition_outputs
+        transitions.append(transition_dict)
+    output['transitions'] = transitions 
+
+    # Initial states are the states that have transitions from Sinit. This block
+    # similar to previous block
+    initial_transitions = mealy_machine.transitions.find(from_states=['Sinit'])
+    initial_states = [ trans[1] for trans in initial_transitions ]
+    initial_trans = []
+    for init_transition in initial_transitions:
+        transition_vals = init_transition[2]
+        trans_dict = {}
+        trans_dict['state'] = init_transition[1]
+        trans_dict['inputs'] = { var: transition_vals[var] for var in env_vars}
+        trans_dict['outputs'] = { var: transition_vals[var] for var in sys_vars}
+        initial_trans.append(trans_dict)
+    output['init_trans'] = initial_trans
 
     return output
