3178c570ee1ef06eb8ace033f205f51743ac54c6
Ioannis Filippidis <jfilippidis@gmail.com>
2014-03-24 04:32:05 -0700

    mv code from abstract to independent polytope package
    plot_partition, plot_transition_arrow, find_adjacent_regions, Partition, MetricPartition, abstract.prop2partition._save_region_plot

--- tulip/abstract/plot.py
+++ tulip/abstract/plot.py
@@ -41,7 +41,7 @@ import numpy as np
 from scipy import sparse as sp
 import networkx as nx
 
-from polytope import cheby_ball
+from polytope import plot_partition, plot_transition_arrow
 
 try:
     import matplotlib as mpl
@@ -54,112 +54,6 @@ try:
 except Exception, e:
     logger.error(e)
     mpl = None
-
-def plot_partition(
-    ppp, trans=None, ppp2trans=None, only_adjacent=False,
-    ax=None, plot_numbers=True, color_seed=None, show=False
-):
-    """Plot partition with arrows from digraph.
-    
-    For filtering edges based on label use L{plot_ts_on_partition}.
-
-    See Also
-    ========
-    L{abstract.prop2partition.PropPreservingPartition}, L{plot_trajectory}
-
-    @type ppp: L{PropPreservingPartition}
-    
-    @param trans: Transition matrix. If used,
-        then transitions in C{ppp} are shown with arrows.
-        Otherwise C{ppp.adj} is plotted.
-        
-        To show C{ppp.adj}, pass: trans = True
-    
-    @param plot_numbers: If True,
-        then annotate each Region center with its number.
-    
-    @param show: If True, then show the plot.
-        Otherwise return axis object.
-        Axis object is good for creating custom plots.
-    
-    @param ax: axes where to plot
-    
-    @param color_seed: seed for reproducible random coloring
-    
-    @param ppp2trans: order mapping ppp indices to trans states
-    @type ppp2trans: list of trans states
-    """
-    if mpl is None:
-        warn('matplotlib not found')
-        return
-    
-    # needs to be converted to adjacency matrix ?
-    if isinstance(trans, nx.MultiDiGraph):
-        if trans is not None and ppp2trans is None:
-            msg = 'trans is a networkx MultiDiGraph, '
-            msg += 'so ppp2trans required to define state order,\n'
-            msg += 'used when converting the graph to an adjacency matrix.'
-            raise Exception(msg)
-        
-        trans = nx.to_numpy_matrix(trans, nodelist=ppp2trans)
-        trans = np.array(trans)
-    
-    l,u = ppp.domain.bounding_box
-    arr_size = (u[0,0]-l[0,0])/50.0
-    
-    # new figure ?
-    if ax is None:
-        ax, fig = newax()
-    
-    # no trans given: use partition's
-    if trans is True and ppp.adj is not None:
-        ax.set_title('Adjacency from Partition')
-        trans = ppp.adj
-    elif trans is None:
-        trans = 'none'
-    else:
-        ax.set_title('Adjacency from given Transitions')
-    
-    ax.set_xlim(l[0,0],u[0,0])
-    ax.set_ylim(l[1,0],u[1,0])
-    
-    # repeatable coloring ?
-    if color_seed is not None:
-        prng = np.random.RandomState(color_seed)
-    else:
-        prng = np.random.RandomState()
-    
-    # plot polytope patches
-    for i, reg in enumerate(ppp.regions):
-        # select random color,
-        # same color for all polytopes in each region
-        col = prng.rand(3)
-        
-        # single polytope or region ?
-        reg.plot(color=col, ax=ax)
-        if plot_numbers:
-            reg.text(str(i), ax, color='red')
-    
-    # not show trans ?
-    if trans is 'none':
-        if show:
-            mpl.pyplot.show()
-        return ax
-    
-    # plot transition arrows between patches
-    rows, cols = np.nonzero(trans)
-    for i, j in zip(rows, cols):
-        # mask non-adjacent cell transitions ?
-        if only_adjacent:
-            if ppp.adj[i, j] == 0:
-                continue
-        
-        plot_transition_arrow(ppp.regions[i], ppp.regions[j], ax, arr_size)
-    
-    if show:
-        mpl.pyplot.show()
-    
-    return ax
 
 def plot_abstraction_scc(ab, ax=None):
     """Plot Regions colored by strongly connected component.
@@ -252,42 +146,6 @@ def plot_strategy(ab, mealy):
     proj_mealy = project_strategy_on_partition(ab.ppp, mealy)
     ax = plot_partition(ab.ppp, proj_mealy, color_seed=0)
     return ax
-
-def plot_transition_arrow(polyreg0, polyreg1, ax, arr_size=None):
-    """Plot arrow starting from polyreg0 and ending at polyreg1.
-    
-    @type polyreg0: L{Polytope} or L{Region}
-    @type polyreg1: L{Polytope} or L{Region}
-    @param ax: axes where to plot
-    
-    @return: arrow object
-    """
-    # brevity
-    p0 = polyreg0
-    p1 = polyreg1
-    
-    rc0, xc0 = cheby_ball(p0)
-    rc1, xc1 = cheby_ball(p1)
-    
-    if np.sum(np.abs(xc1-xc0)) < 1e-7:
-        return None
-    
-    if arr_size is None:
-        l,u = polyreg1.bounding_box
-        arr_size = (u[0,0]-l[0,0])/25.0
-    
-    #TODO: 3d
-    x = xc0[0]
-    y = xc0[1]
-    dx = xc1[0] - xc0[0]
-    dy = xc1[1] - xc0[1]
-    arrow = mpl.patches.Arrow(
-        float(x), float(y), float(dx), float(dy),
-        width=arr_size, color='black'
-    )
-    ax.add_patch(arrow)
-    
-    return arrow
 
 def plot_trajectory(ppp, x0, u_seq, ssys,
                     ax=None, color_seed=None):
--- tulip/abstract/prop2partition.py
+++ tulip/abstract/prop2partition.py
@@ -41,7 +41,6 @@ import copy
 
 import numpy as np
 from scipy import sparse as sp
-import networkx as nx
 
 import polytope as pc
 from .plot import plot_partition
@@ -141,7 +140,7 @@ def prop2part(state_space, cont_props_dict):
         prop_regions = copy.deepcopy(cont_props_dict)
     )
     
-    mypartition.adj = find_adjacent_regions(mypartition).copy()
+    mypartition.adj = pc.find_adjacent_regions(mypartition).copy()
     
     return mypartition
 
@@ -174,7 +173,7 @@ def part2convex(ppp):
             cvxpart.regions.append(region_now)
             new2old += [i]
     
-    cvxpart.adj = find_adjacent_regions(cvxpart).copy()
+    cvxpart.adj = pc.find_adjacent_regions(cvxpart).copy()
     
     return (cvxpart, new2old)
     
@@ -427,261 +426,9 @@ def product_interval(list1, list2):
             new_list.append(list1[m]+list2[n])
     return new_list
 
-def find_adjacent_regions(partition):
-    """Return region pairs that are spatially adjacent.
-    
-    @type partition: iterable container of L{Region}
-    
-    @rtype: lil_matrix
-    """
-    n = len(partition)
-    adj = sp.lil_matrix((n, n), dtype=np.int8)
-    s = partition.regions
-    
-    for i, a in enumerate(s):
-        adj[i, i] = 1
-        
-        for j, b in enumerate(s[0:i]):
-            adj[i, j] = adj[j, i] = pc.is_adjacent(a, b)
-        
-    return adj
-
 ################################
 
-class Partition(object):
-    """Partition of a set.
-    
-    A C{Partition} is an iterable container of sets
-    over C{Partition.set} and these must implement the methods:
-    
-        - union, __add__
-        - difference
-        - intersection
-        - __le__
-    
-    so the builtin class C{set} can be used for discrete sets,
-    or custom classes (e.g. polytopes) can be used for sets
-    equipped with more structure.
-    
-    To utilize additional structure, see L{MetricPartition}.
-    """
-    def __init__(self, domain=None):
-        """Partition over C{domain}.
-        
-        C{domain} is used to avoid conflicts with
-        the python builtin set function.
-        """
-        self.set = domain
-    
-    def __len__(self):
-        return len(self.regions)
-    
-    def __iter__(self):
-        return iter(self.regions)
-    
-    def __getitem__(self, key):
-        return self.regions[key]
-    
-    def is_partition(self):
-        """Return True if Regions are pairwise disjoint and cover domain.
-        """
-        return self.is_cover() and self.are_disjoint()
-    
-    def is_cover(self):
-        """Return True if Regions cover domain
-        """
-        union = pc.Region()
-        for region in self.regions:
-            union += region
-        
-        if not self.domain <= union:
-            msg = 'partition does not cover domain.'
-            logger.Error(msg)
-            warnings.warn(msg)
-            return False
-        else:
-            return True
-    
-    def are_disjoint(self, check_all=False, fname=None):
-        """Return True if all Regions are disjoint.
-        
-        Print:
-        
-            - the offending Regions and their
-            - their intersection (mean) volume ratio
-            - their difference (mean) volume ratio
-        
-        Optionally save numbered figures of:
-        
-            - offending Regions
-            - their intersection
-            - their difference
-        
-        @param check_all: don't return when first offending regions found,
-            continue and check all pairs
-        @type check_all: bool
-        
-        @param fname: path prefix where to save the debugging figures
-            By default no figures are saved.
-        @type fname: str
-        """
-        logger.info('checking if PPP is a partition.')
-        
-        l,u = self.set.bounding_box
-        ok = True
-        for i, region in enumerate(self.regions):
-            for j, other in enumerate(self.regions[0:i]):
-                if pc.is_fulldim(region.intersect(other) ):
-                    msg = 'PPP is not a partition, regions: '
-                    msg += str(i) + ' and: ' + str(j)
-                    msg += ' intersect each other.\n'
-                    msg += 'Offending regions are:\n' + 10*'-' + '\n'
-                    msg += str(region) + 10*'-' + '\n'
-                    msg += str(other) + 10*'-' + '\n'
-                    
-                    isect = region.intersect(other)
-                    diff = region.diff(other)
-                    
-                    mean_volume = (region.volume + other.volume) /2.0
-                    
-                    overlap = 100 * isect.volume / mean_volume
-                    non_overlap = 100 * diff.volume / mean_volume
-                    
-                    msg += '|cap| = ' + str(overlap) + ' %\n'
-                    msg += '|diff| = ' + str(non_overlap) + '\n'
-                    
-                    logger.error(msg)
-                    
-                    if fname:
-                        print('saving')
-                        fname1 = fname + 'region' + str(i) + '.pdf'
-                        fname2 = fname + 'region' + str(j) + '.pdf'
-                        fname3 = fname + 'isect_' + str(i) + '_' + str(j) + '.pdf'
-                        fname4 = fname + 'diff_' + str(i) + '_' + str(j) + '.pdf'
-                        
-                        _save_region_plot(region, fname1, l, u)
-                        _save_region_plot(other, fname2, l, u)
-                        _save_region_plot(isect, fname3, l, u)
-                        _save_region_plot(diff, fname4, l, u)
-                    
-                    ok = False
-                    if not check_all:
-                        break
-        return ok
-    
-    def refines(self, other):
-        """Return True if each element is a subset of other.
-        
-        @type other: PropPreservingPartition
-        """
-        for small in self:
-            found_superset = False
-            for big in other:
-                if small <= big:
-                    found_superset = True
-                    break
-            if not found_superset:
-                return False
-        return True
-    
-    def preserves(self, other):
-        """Return True if it refines closure of C{other} under complement.
-        
-        Closure under complement is the union of C{other}
-        with the collection of complements of its elements.
-        
-        This method checks the annotation of elements in C{self}
-        with elements fro C{other}.
-        """
-        for item in self._elements:
-            # item subset of these sets
-            for superset in item.supersets:
-                if not item <= superset:
-                    return False
-            
-            # item subset of the complements of these sets
-            for other_set in set(other).difference(item.supersets):
-                if item.intersect(other_set):
-                    return False
-        return True
-
-class MetricPartition(Partition, nx.Graph):
-    """Partition of a metric space.
-    
-    Includes adjacency information which abstracts
-    the topology induced by the metric.
-    
-    Two subsets in the partition are called adjacent
-    if the intersection of their closure is non-empty.
-    
-    If the space is also a measure space,
-    then volume information is used for diagnostic purposes.
-    """
-    def compute_adj(self):
-        """Update the adjacency matrix by checking all region pairs.
-        
-        Uses L{polytope.is_adjacent}.
-        """
-        n = len(self.regions)
-        adj = sp.lil_matrix((n, n))
-        
-        logger.info('computing adjacency from scratch...')
-        for i, region0 in enumerate(self.regions):
-            for j, region1 in enumerate(self.regions):
-                if i == j:
-                    adj[i, j] = 1
-                    continue
-                
-                if pc.is_adjacent(region0, region1):
-                    adj[i, j] = 1
-                    adj[j, i] = 1
-                    
-                    logger.info('regions: ' + str(i) + ', ' +
-                                 str(j) + ', are adjacent.')
-        logger.info('...done computing adjacency.')
-        
-        # check previous one to unmask errors
-        if self.adj is not None:
-            logger.info('checking previous adjacency...')
-            
-            ok = True
-            row, col = adj.nonzero()
-            
-            for i, j in zip(row, col):
-                assert(adj[i, j])
-                if adj[i, j] != self.adj[i, j]:
-                    ok = False
-                    
-                    msg = 'PPP adjacency matrix is incomplete, '
-                    msg += 'missing: (' + str(i) + ', ' + str(j) + ')'
-                    logger.error(msg)
-            
-            row, col = self.adj.nonzero()
-            
-            for i, j in zip(row, col):
-                assert(self.adj[i, j])
-                if adj[i, j] != self.adj[i, j]:
-                    ok = False
-                    
-                    msg = 'PPP adjacency matrix is incorrect, '
-                    msg += 'has 1 at: (' + str(i) + ', ' + str(j) + ')'
-                    logger.error(msg)
-            
-            if not ok:
-                logging.error('PPP had incorrect adjacency matrix.')
-            
-            logger.info('done checking previous adjacency.')
-        else:
-            ok = True
-            logger.info('no previous adjacency found: ' +
-                        'skip verification.')
-        
-        # update adjacency
-        self.adj = adj
-        
-        return ok
-
-class PropPreservingPartition(MetricPartition):
+class PropPreservingPartition(pc.MetricPartition):
     """Partition class with following fields:
     
       - domain: the domain we want to partition
@@ -880,9 +627,3 @@ class PPP(PropPreservingPartition):
     """
     def __init__(self, **args):
         PropPreservingPartition.__init__(self, **args)
-
-def _save_region_plot(region, fname, l, u):
-    ax = region.plot()
-    ax.set_xlim(l[0,0], u[0,0])
-    ax.set_ylim(l[1,0], u[1,0])
-    ax.figure.savefig(fname)
