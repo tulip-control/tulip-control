8585a004566c440ccdc3f5cfd9914727b4cefdb6
Scott Livingston <slivingston@cds.caltech.edu>
2014-05-13 11:40:32 -0700

    BUILD: Explain version string generation in code comments

--- tulip/version.py
+++ tulip/version.py
@@ -37,8 +37,13 @@ version_info = (1, 1, 'a')
 version = '.'.join([str(x) for x in version_info[:2] ])
 version += version_info[2]
 
-import os.path
 
+# Append annotation to version string to indicate development versions.
+#
+# An empty (modulo comments and blank lines) commit_hash.txt is used
+# to indicate a release, in which case nothing is appended to version
+# string as defined above.
+import os.path
 path_to_hashfile = os.path.join(os.path.dirname(__file__), "commit_hash.txt")
 if os.path.exists(path_to_hashfile):
     commit_hash = ""
@@ -46,9 +51,14 @@ if os.path.exists(path_to_hashfile):
         for line in f:
             line = line.strip()
             if len(line) == 0 or line[0] == '#':
+                # Ignore blank lines and comments, the latter being
+                # any line that begins with #.
                 continue
+
+            # First non-blank line is assumed to be the commit hash
             commit_hash = line
             break
+
     if len(commit_hash) > 0:
         version += "-dev-" + commit_hash
 else:
