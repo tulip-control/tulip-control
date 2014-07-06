cc2d0e9603ffdc71daeb9698bb71c31845874f72
Ioannis Filippidis <jfilippidis@gmail.com>
2014-07-06 14:27:08 -0700

    docstring style

--- tulip/transys/machines.py
+++ tulip/transys/machines.py
@@ -323,8 +323,8 @@ class FiniteStateMachine(LabeledDiGraph):
         raise NotImplementedError
 
 class FSM(FiniteStateMachine):
-    """Alias for Finite-state Machine."""
-    
+    """Alias for Finite-state Machine.
+    """
     def __init__(self, **args):
         FiniteStateMachine.__init__(self, **args)
 
