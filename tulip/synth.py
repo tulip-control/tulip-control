tulip-control/tulip-control

commit 598d43e24a870f48d47de35a6e5d49e3d28c967d

rename synth.synthesize option: trim_aut -> rm_deadends
- because the object returned is a transducer, not an acceptor.
- refer to the result in the docstring (w/o details about removal iterations)
- bugfix: call the renamed LabeledDiGraph.remove_deadends

tulip/synth.py
@@ -1070,7 +1070,7 @@ def synthesize(
     option, specs, env=None, sys=None,
     ignore_env_init=False, ignore_sys_init=False,
     bool_states=False, action_vars=None,
-    bool_actions=False, trim_aut=True
+    bool_actions=False, rm_deadends=True
 ):
     """Function to call the appropriate synthesis tool on the specification.
 
@@ -1140,9 +1140,9 @@ def synthesize(
     @param bool_actions: model actions using bool variables
     @type bool_actions: bool
 
-    @param trim_aut: if True, 
-        then remove all states without outgoing transitions
-    @type trim_aut: bool
+    @param rm_deadends: if True,
+        then the returned strategy contains no terminal states.
+    @type rm_deadends: bool
     
     @return: If spec is realizable,
         then return a Mealy machine implementing the strategy.
@@ -1179,7 +1179,7 @@ def synthesize(
         return None
 
     if trim_aut:
-        ctrl.trim_dead_states()
+        ctrl.remove_deadends()
 
     return ctrl
 

commit 26a166b83966e9a9c83a314fc468a104c2682628

BUGFIX: undefined "trim_aut" in synth.synthesize()
Bug apparently introduced in 598d43e24a870f48d47de35a6e5d49e3d28c967d
Corresponding test is given in previous commit (254a148176727b22de1544b5db897eaf6e51fda7).

tulip/synth.py
@@ -1178,7 +1178,7 @@ def synthesize(
     if not isinstance(ctrl, transys.MealyMachine):
         return None
 
-    if trim_aut:
+    if rm_deadends:
         ctrl.remove_deadends()
 
     return ctrl