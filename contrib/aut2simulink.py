0b7ea5aab60c02e42deb91e7c18f23160e2eeea8
Scott Livingston <slivingston@cds.caltech.edu>
2014-08-27 09:53:19 -0400

    Add copyright notice for aut2simulink.py

--- contrib/aut2simulink.py
+++ contrib/aut2simulink.py
@@ -1,18 +1,49 @@
-import re, copy, os, random, sys
-import xml.etree.ElementTree as ET
-import Queue
-import exceptions
-
-
-#Written by Robert Rogersten during SURF June 2012,
-#Co-mentors Mumu Xu, Necmiye Ozay and Ufuk Topcu.
-
+# Copyright (c) 2012 by California Institute of Technology
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the California Institute of Technology nor
+#    the names of its contributors may be used to endorse or promote
+#    products derived from this software without specific prior
+#    written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CALTECH
+# OR THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
 """
 This program takes an aut and smv file from a generated TuLiP controller
 and automatically writes the MATLAB compatible script for that controller.
 Run this program by typing "python programfile.py nameofautfile nameofmatlabfile", aut and smv file shall have the same name.
 Do not include file extensions.
+
+Written by Robert Rogersten during SURF June 2012,
+Co-mentors Mumu Xu, Necmiye Ozay and Ufuk Topcu.
 """
+
+import re, copy, os, random, sys
+import xml.etree.ElementTree as ET
+import Queue
+import exceptions
+
 
 class AutomatonState:
     """AutomatonState class for representing a state in a finite state
