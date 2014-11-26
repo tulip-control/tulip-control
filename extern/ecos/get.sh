985cc24e88e07ccec0b860a811bb6d8657bb328e
Scott Livingston <slivingston@cds.caltech.edu>
2014-11-26 12:22:25 -0500

    EXTERN: DOC, and return error if checksums do not match for ECOS
    The intent is that get.sh should only return with success if it
    actually had success, which is important if get.sh is used by something
    else automatically.
    
    The README file together with error messages or comments in get.sh
    should be enough for users with little or no experience with shell
    scripting.

--- extern/ecos/get.sh
+++ extern/ecos/get.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -e
 #
 # Get and patch ECOS version 1.0.4
+#
+# Original Copies of patched files have names with suffix of ".orig"
+# Consult README in this same directory for help and details.
 
 wget https://github.com/embotech/ecos/archive/v1.0.4.tar.gz
 MD5CHECKSUM=9846bfa7817d3669f3c9269bf5567811
@@ -11,6 +14,6 @@ then
 	cd ecos-1.0.4
 	sed -i.orig '10s/-Wextra/-Wextra -fPIC/' ecos.mk
 else
-	echo "The checksums do not match"
+	echo "The checksums do not match.  Please consult the README."
+	false  # Quit indicating error
 fi
-
