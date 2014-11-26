e35517757f162902b59fd2d60ecc0d70f10106fa
Scott Livingston <slivingston@cds.caltech.edu>
2014-11-26 10:06:44 -0500

    EXTERN: Make ECOS URLs consistent, under github.com/embotech

--- extern/ecos/install.sh
+++ extern/ecos/install.sh
@@ -1,5 +1,5 @@
 # install ecos-1.0.4
-wget https://github.com/ifa-ethz/ecos/archive/v1.0.4.tar.gz
+wget https://github.com/embotech/ecos/archive/v1.0.4.tar.gz
 MD5CHECKSUM=9846bfa7817d3669f3c9269bf5567811
 FILECHECKSUM=`md5sum v1.0.4.tar.gz| sed 's/ .*//'`
 if [ "$MD5CHECKSUM" = "$FILECHECKSUM" ]
