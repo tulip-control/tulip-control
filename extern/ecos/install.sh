95b9645ce4c15e805e167867dd5d3bc09ea6add6
Scott Livingston <slivingston@cds.caltech.edu>
2014-11-26 11:18:27 -0500

    EXTERN: ecos: Place fetched files under separate dir; quit on error
    Two important modifications to the shell script:
    1. quit upon error, including missing tools
    2. uses `/bin/sh` unless invoked by passing to a shell program.

--- extern/ecos/install.sh
+++ extern/ecos/install.sh
@@ -1,11 +1,15 @@
-# install ecos-1.0.4
+#!/bin/sh -e
+#
+# Get and patch ECOS version 1.0.4
+
 wget https://github.com/embotech/ecos/archive/v1.0.4.tar.gz
 MD5CHECKSUM=9846bfa7817d3669f3c9269bf5567811
 FILECHECKSUM=`md5sum v1.0.4.tar.gz| sed 's/ .*//'`
 if [ "$MD5CHECKSUM" = "$FILECHECKSUM" ]
 then
-	tar xzf v1.0.4.tar.gz --strip 1
-	sed -i '10s/-Wextra/-Wextra -fPIC/' ecos.mk
+	tar xzf v1.0.4.tar.gz
+	cd ecos-1.0.4
+	sed -i.orig '10s/-Wextra/-Wextra -fPIC/' ecos.mk
 	make ecos
 else
 	echo "The checksums do not match"
