a7859c0d9c6a5ac86269181eaffad68788b41010
Scott Livingston <slivingston@cds.caltech.edu>
2014-11-29 21:53:32 -0500

    CONTRIB: Add copyright statement and reference of original work
    ...for stairsB.c from the Ptolemy repository on which TuLiPFMU.c is based.

--- contrib/fmu/sources/TuLiPFMU.c
+++ contrib/fmu/sources/TuLiPFMU.c
@@ -3,10 +3,42 @@
  *
  * This file is based on the template FMU 'stepCounter' developed by
  * Christopher Brooks and Edward A. Lee and the template FMU 'stairsB'
- * developed by David Broman
+ * developed by David Broman.  The latter can be found at
+ *   trunk/ptolemy/actor/lib/fmi/fmus/stairsB/src/sources/stairsB.c
+ * in the SVN repository
+ *   https://repo.eecs.berkeley.edu/svn-anon/projects/eal/ptII
+ * as of revision 69602.  The corresponding copyright statement and
+ * license for that work immediately follow this comment block.
+ *
  *
  * Authors: Yilin Mo
  * ---------------------------------------------------------------------------*/
+/*
+Below is the copyright agreement for the Ptolemy II system.
+Version: $Id: copyright.txt 68472 2014-02-24 22:53:44Z cxh $
+
+Copyright (c) 2013, 2014 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+*/
 
 
 #include <stdio.h>
