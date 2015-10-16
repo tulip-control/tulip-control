0fc0fa46ea2b4053f9ae1e4fdd9e228477062441
Scott Livingston <slivingston@cds.caltech.edu>
2015-10-16 08:25:02 -0700

    EXAMPLE: Strings shall not be used as code comments

--- examples/developer/fuel_tank/double_tank.py
+++ examples/developer/fuel_tank/double_tank.py
@@ -14,6 +14,9 @@ to T1 at a constant rate.
 The objective is to keep the fuel levels in
 the tanks close to each other.
 
+CODE KEY:
+- ## : beginning of major step; e.g., dynamics definition
+
 reference
 =========
     Nilsson P.; Ozay N.; Topcu U.; Murray R.M.
@@ -44,14 +47,14 @@ import polytope as pc
 from tulip.abstract.plot import plot_strategy
 #from tulip.graphics import newax
 
-"""Problem variables"""
+## Problem variables
 tank_capacity = 10      # Maximum tank capacity
 fuel_consumption = 1    # Rate at which fuel is drawn from tank 2
 refill_rate = 3         # Rate at which fuel is refilled in tank 1 during refuel mode
 input_lb = 0            # Lower bound on fuel move rate from tank 1 to 2
 input_ub = 3            # Upper bound on fuel move rate from tank 1 to 2
 max_vol_diff = 2        # Enforce |v1-v2| to be lower than this value
-fin_vol_diff = 1        # Enforce |v1-v2| to alw. ev. be lower than this value
+fin_vol_diff = 1        # Enforce |v1-v2| to always eventually. be lower than this value
 max_refuel_level = 8    # Above this level no refueling will take place
 N = 1                   # Horizon length
 disturbance = 0.0       # Absolute uncertainty in fuel consumption
@@ -62,7 +65,7 @@ fast = True             # Use smaller domain to increase speed
 imgpath = './'
 fontsize = 18
 
-"""State space and propositions"""
+## State space and propositions
 if fast:
     cont_ss = pc.Polytope(
         np.array([[1,0],
@@ -119,7 +122,7 @@ cont_props['critical'] = pc.Polytope(
     np.array([0, 0, 2*fuel_consumption])
 )
 
-"""Dynamics"""
+## Dynamics
 A = np.eye(2)
 B = np.array([[-1],[1]])
 E = np.array([[0],[1]])
@@ -150,7 +153,7 @@ cont_dyn_normal = hybrid.LtiSysDyn(A, B, E, K1, U1, W, domain=cont_ss)
 # Aerial refueling mode dynamics
 cont_dyn_refuel = hybrid.LtiSysDyn(A, B, E, K2, U2, W, domain=cont_ss)
 
-"""Switched Dynamics"""
+## Switched Dynamics
 env_modes = ('normal', 'refuel')
 sys_modes = ('fly',)
 
@@ -170,7 +173,7 @@ switched_dynamics = hybrid.SwitchedSysDyn(
     disc_sys_labels=sys_modes
 )
 
-"""Create convex proposition preserving partition"""
+## Create convex proposition preserving partition
 ppp = abstract.prop2part(cont_ss, cont_props)
 ppp, new2old = abstract.part2convex(ppp)
 
@@ -180,7 +183,7 @@ ax.figure.savefig(imgpath + 'cprops.pdf')
 ax = ppp.plot()
 ax.figure.savefig(imgpath + 'ppp.pdf')
 
-"""Discretize to establish transitions"""
+## Discretize to establish transitions
 if os.name == "posix":
     start = os.times()[2]
     logger.info('start time: ' + str(start))
@@ -192,7 +195,7 @@ disc_params[('normal', 'fly')] = {'N':N, 'trans_length':3}
 disc_params[('refuel', 'fly')] = {'N':N, 'trans_length':3}
 
 sys_ts = abstract.multiproc_discretize_switched(
-    ppp, switched_dynamics, disc_params, plot=False
+    ppp, switched_dynamics, disc_params, plot=True
 )
 
 if os.name == "posix":
@@ -201,13 +204,13 @@ if os.name == "posix":
     elapsed = (end - start)
     logger.info('Discretization lasted: ' + str(elapsed))
 
-"""Save abstraction to save debugging time"""
+## Save abstraction to save debugging time
 fname = './abstract_switched.pickle'
 #pickle.dump(sys_ts, open(fname, 'wb') )
 
 #sys_ts = pickle.load(open(fname, 'r') )
 
-"""Specs"""
+## Specifications
 env_vars = set()
 sys_disc_vars = set()
 
@@ -231,7 +234,7 @@ specs = spec.GRSpec(env_vars, sys_disc_vars,
                     env_prog, sys_prog)
 print(specs.pretty())
 
-"""Synthesis"""
+## Synthesis
 print("Starting synthesis")
 if os.name == "posix":
     start = os.times()[2]
@@ -251,81 +254,80 @@ ctrl.save(imgpath + 'double_tank.pdf')
 ax = plot_strategy(sys_ts, ctrl)
 ax.figure.savefig(imgpath + 'proj_mealy.pdf')
 
-"""Simulate""
-num_it = 25
-init_state = {}
-init_state['u_in'] = 0
+## Simulate
+# num_it = 25
+# init_state = {}
+# init_state['u_in'] = 0
 
-destfile = 'rsdisturbance_example.gexf'
-states = grsim.grsim(
-    [aut], env_states=[init_state], num_it=num_it,
-    deterministic_env=False, graph_vis=False,
-    destfile=destfile
-)
+# destfile = 'rsdisturbance_example.gexf'
+# states = grsim.grsim(
+#     [aut], env_states=[init_state], num_it=num_it,
+#     deterministic_env=False, graph_vis=False,
+#     destfile=destfile
+# )
 
-uin_arr = []
-cellid_arr = []
-for (autID, state) in states:
-    uin_arr.append(state.state['u_in'])
-    cellid_arr.append(state.state['cellID'])
-uin_arr = np.array(uin_arr)
-cellid_arr = np.array(cellid_arr)
+# uin_arr = []
+# cellid_arr = []
+# for (autID, state) in states:
+#     uin_arr.append(state.state['u_in'])
+#     cellid_arr.append(state.state['cellID'])
+# uin_arr = np.array(uin_arr)
+# cellid_arr = np.array(cellid_arr)
 
-rc, x = pc.cheby_ball(new_part.regions[cellid_arr[0]])
-x = x.flatten()
-x_arr = x.copy()
-u_arr = np.zeros(1)
-for i in range(1, len(cellid_arr)):
-    if uin_arr[i-1] == 0:
-        u = abstract.get_input(
-            x, cont_dyn_normal, new_part,
-            cellid_arr[i-1], cellid_arr[i], 1, mid_weight=10
-        )
-        x = np.dot(cont_dyn_normal.A,x).flatten() + \
-            np.dot(cont_dyn_normal.B,u).flatten() + \
-            cont_dyn_normal.K.flatten()
-    else:
-        u = abstract.get_input(
-            x, cont_dyn_refuel, new_part, cellid_arr[i-1],
-            cellid_arr[i], 1, Q=[], mid_weight=10
-        )
-        x = np.dot(cont_dyn_refuel.A,x).flatten() + \
-            np.dot(cont_dyn_refuel.B,u).flatten() + \
-            cont_dyn_refuel.K.flatten()
+# rc, x = pc.cheby_ball(new_part.regions[cellid_arr[0]])
+# x = x.flatten()
+# x_arr = x.copy()
+# u_arr = np.zeros(1)
+# for i in range(1, len(cellid_arr)):
+#     if uin_arr[i-1] == 0:
+#         u = abstract.get_input(
+#             x, cont_dyn_normal, new_part,
+#             cellid_arr[i-1], cellid_arr[i], 1, mid_weight=10
+#         )
+#         x = np.dot(cont_dyn_normal.A,x).flatten() + \
+#             np.dot(cont_dyn_normal.B,u).flatten() + \
+#             cont_dyn_normal.K.flatten()
+#     else:
+#         u = abstract.get_input(
+#             x, cont_dyn_refuel, new_part, cellid_arr[i-1],
+#             cellid_arr[i], 1, Q=[], mid_weight=10
+#         )
+#         x = np.dot(cont_dyn_refuel.A,x).flatten() + \
+#             np.dot(cont_dyn_refuel.B,u).flatten() + \
+#             cont_dyn_refuel.K.flatten()
     
-    u_arr = np.hstack([u_arr, u.flatten()])
-    x_arr = np.vstack([x_arr, x])
+#     u_arr = np.hstack([u_arr, u.flatten()])
+#     x_arr = np.vstack([x_arr, x])
     
-data = {}
-data['x'] = x_arr
-data['u'] = u_arr[range(1,u_arr.shape[0]), :]
-data['u_in'] = uin_arr
+# data = {}
+# data['x'] = x_arr
+# data['u'] = u_arr[range(1,u_arr.shape[0]), :]
+# data['u_in'] = uin_arr
 
-sio.savemat("matlabdata", data)
+# sio.savemat("matlabdata", data)
 
-ax = plot_partition(new_part, plot_numbers=False, show=False)
-for tick in ax.xaxis.get_major_ticks():
-    tick.label1.set_fontsize(fontsize)
-for tick in ax.yaxis.get_major_ticks():
-    tick.label1.set_fontsize(fontsize)
-plt.xlabel('$v_1$', fontsize=fontsize+6)
-plt.ylabel('$v_2$', fontsize=fontsize+6)
+# ax = plot_partition(new_part, plot_numbers=False, show=False)
+# for tick in ax.xaxis.get_major_ticks():
+#     tick.label1.set_fontsize(fontsize)
+# for tick in ax.yaxis.get_major_ticks():
+#     tick.label1.set_fontsize(fontsize)
+# plt.xlabel('$v_1$', fontsize=fontsize+6)
+# plt.ylabel('$v_2$', fontsize=fontsize+6)
 
-# Plot transitions
-arr_size=0.2
-for i in range(1,x_arr.shape[0]):
-    x = x_arr[i-1,0]
-    y = x_arr[i-1,1]
-    dx = x_arr[i,0] - x
-    dy = x_arr[i,1] - y
-    arr = matplotlib.patches.Arrow(
-        float(x), float(y), float(dx),
-        float(dy), width=arr_size
-    )
-    ax.add_patch(arr)
+# # Plot transitions
+# arr_size=0.2
+# for i in range(1,x_arr.shape[0]):
+#     x = x_arr[i-1,0]
+#     y = x_arr[i-1,1]
+#     dx = x_arr[i,0] - x
+#     dy = x_arr[i,1] - y
+#     arr = matplotlib.patches.Arrow(
+#         float(x), float(y), float(dx),
+#         float(dy), width=arr_size
+#     )
+#     ax.add_patch(arr)
 
-ax.plot(x_arr[0,0], x_arr[0,1], 'og')
-ax.plot(x_arr[-1,0], x_arr[-1,1], 'or')
+# ax.plot(x_arr[0,0], x_arr[0,1], 'og')
+# ax.plot(x_arr[-1,0], x_arr[-1,1], 'or')
 
-plt.savefig(imgpath + 'simulation.pdf')
-"""
+# plt.savefig(imgpath + 'simulation.pdf')
