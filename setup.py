1ad10295d780a44d1bdda0f8e35f10f1893e4b40
Ioannis Filippidis <jfilippidis@gmail.com>
2016-01-03 05:50:33 +0200

    DEP: rm infrastructure for checking dependencies
    most of it was necessary when more heterogeneous and non-Python dependencies where required.
    Since then, `pip` has improved noticeably, so it can serve this purpose for the Python dependencies.
    
    Each module in `tulip.interfaces` is responsible for checking the presence of the tool it communicates with.
    This arrangement is local and simpler.

--- setup.py
+++ setup.py
@@ -20,41 +20,6 @@ classifiers = [
     'Topic :: Scientific/Engineering']
 
 
-###########################################
-# Dependency or optional-checking functions
-###########################################
-# (see notes below.)
-
-
-
-# Handle "dry-check" argument to check for dependencies without
-# installing the tulip package; checking occurs by default if
-# "install" is given, unless both "install" and "nocheck" are given
-# (but typical users do not need "nocheck").
-
-
-
-# You *must* have these to run TuLiP.  Each item in other_depends must
-# be treated specially; thus other_depends is a dictionary with
-#
-#   keys   : names of dependency;
-
-#   values : list of callable and string, which is printed on failure
-#           (i.e. package not found); we interpret the return value
-#           True to be success, and False failure.
-other_depends = dict()
-
-
-
-# These are nice to have but not necessary. Each item is of the form
-#
-#   keys   : name of optional package;
-#   values : list of callable and two strings, first string printed on
-#           success, second printed on failure (i.e. package not
-#           found); we interpret the return value True to be success,
-#           and False failure.
-optionals = dict()
-
 def retrieve_git_info():
     """Return commit hash of HEAD, or "release", or None if failure.
 
@@ -103,52 +68,14 @@ def retrieve_git_info():
     return sha1
 
 
-perform_setup = True
-check_deps = False
-if (
-    ('install' in sys.argv[1:] or 'develop' in sys.argv[1:]) and
-    'nocheck' not in sys.argv[1:]
-):
-    check_deps = True
-elif 'dry-check' in sys.argv[1:]:
-    perform_setup = False
-    check_deps = True
-
-# Pull "dry-check" and "nocheck" from argument list, if present, to play
-# nicely with Distutils setup.
-try:
-    sys.argv.remove('dry-check')
-except ValueError:
-    pass
-try:
-    sys.argv.remove('nocheck')
-except ValueError:
-    pass
-
 package_data = {
     'tulip': ['commit_hash.txt'],
     'tulip.transys.export' : ['d3.v3.min.js'],
     'tulip.spec' : ['parsetab.py']
 }
 
-if check_deps:
-    # Other dependencies
-    for (dep_key, dep_val) in other_depends.items():
-        print('Probing for required dependency:' + dep_key + '...')
-        if dep_val[0]():
-            print('\t' + dep_val[1])
-        else:
-            print('\t' + dep_val[2])
-            raise Exception('Failed dependency: '+dep_key)
 
-    # Optional stuff
-    for (opt_key, opt_val) in optionals.items():
-        print('Probing for optional '+opt_key+'...')
-        if opt_val[0]():
-            print("\t"+opt_val[1] )
-        else:
-            print("\t"+opt_val[2] )
-
+def package_jtlv():
     # Optional stuff for which the installation configuration will
     # change depending on the availability of each.
     if os.path.exists(os.path.join('tulip', 'interfaces', 'jtlv_grgame.jar')):
@@ -161,7 +88,7 @@ if check_deps:
               'the default GR(1) solver of TuLiP.')
 
 
-if perform_setup:
+def run_setup():
     # Build PLY table, to be installed as tulip package data
     try:
         import tulip.spec.lexyacc
