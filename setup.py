ba3d578debb9df0b81d53c8afc49df4746065e38
Ioannis Filippidis <jfilippidis@gmail.com>
2016-06-15 19:37:09 -0700

    MAI: assert that a release is tagged as `'v' + version`
    This checks that `repo.git.describe` returns tags matching the given
    regular expression.
    Also makes the assertion more readable, by prepending, instead of slicing.

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ def git_version(version):
     except git.GitCommandError:
         return '{v}.dev0+{sha}'.format(
             v=version, sha=sha)
-    assert tag[1:] == version, (tag, version)
+    assert tag == 'v' + version, (tag, version)
     return version
 
 
