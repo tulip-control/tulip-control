7647d64607c731f060a223a4e121f5be3ec34678
Ioannis Filippidis <jfilippidis@gmail.com>
2016-09-01 15:10:06 -0700

    DEV: fetch slugs tarball for specific commit using `curl`
    as documented here:
        https://developer.github.com/v3/repos/contents/#get-archive-link

--- extern/get-slugs.sh
+++ extern/get-slugs.sh
@@ -7,8 +7,9 @@
 # and the converter to slugsin format is
 # slugs-repo/tools/StructuredSlugsParser/compiler.py
 
-git clone --depth=10 https://github.com/LTLMoP/slugs.git slugs-repo
-cd slugs-repo
-git checkout ad0cf12c14131fc6a20fe29edfe04d9aefd7c6d4
-cd src
-make
+curl -L https://github.com/VerifiableRobotics/slugs/tarball/ad0cf12c14131fc6a20fe29edfe04d9aefd7c6d4 -o slugs.tar.gz
+echo '04c8de023bddc6579fe5f1fe495dea641d3e4cf25bd48ff3a6b0a781d968136a6a635e07c9c1fcf0c784df60b72c24aab8029f0c613c71fed9c104f0e9f046ce  slugs.tar.gz' | shasum -a 512 -c -
+mkdir slugs-repo
+tar xzf slugs.tar.gz -C slugs-repo --strip=1
+cd slugs-repo/src
+make -j2
