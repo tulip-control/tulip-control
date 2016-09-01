6300fa91d7731d0c03d65652f14abdef15a15a0c
Ioannis Filippidis <jfilippidis@gmail.com>
2016-09-01 03:13:46 -0700

    fixup! TEST: Move synth tests that use opt dependencies
    PEP8 docstring, imports at top

--- tests/synth_extra_test.py
+++ tests/synth_extra_test.py
@@ -1,11 +1,10 @@
-"""
-Tests for the tulip.synth module when gr1c is used.
-"""
+"""Tests for the tulip.synth module when gr1c is used."""
 import logging
+from synth_test import multiple_env_actions_check
+
+
 logging.getLogger('tulip').setLevel(logging.ERROR)
 logging.getLogger('tulip.interfaces.gr1c').setLevel(logging.DEBUG)
-
-from synth_test import multiple_env_actions_check
 
 
 def multiple_env_actions_test():
