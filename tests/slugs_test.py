ea50e668758bb1165ef6a8476492cb47529e9002
Ioannis Filippidis <jfilippidis@gmail.com>
2016-11-25 22:09:58 -0800

    REF: cp to `slugs` tests any code used from `jtlv` tests
    to decouple the unit tests of each interface.

--- tests/slugs_test.py
+++ tests/slugs_test.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 """Tests for the interface with slugs."""
 import logging
-logger = logging.getLogger(__name__)
+
+import networkx as nx
 from tulip.interfaces import slugs
-import jtlvint_test
+from tulip.spec import GRSpec
+
+
+logger = logging.getLogger(__name__)
 
 
 def bitfields_to_ints_test():
@@ -36,8 +40,60 @@ def bitfields_to_ints_test():
         assert m == {'a': n}
 
 
-class basic_test(jtlvint_test.basic_test):
+class basic_test(object):
     def setUp(self):
-        super(basic_test, self).setUp()
         self.check_realizable = lambda x: slugs.synthesize(x) is not None
         self.synthesize = slugs.synthesize
+        self.f_un = GRSpec(env_vars="x", sys_vars="y",
+                           env_init="x", env_prog="x",
+                           sys_init="y", sys_safety=["y -> X(!y)", "!y -> X(y)"],
+                           sys_prog="y && x",
+                           moore=False, plus_one=False, qinit='\A \E')
+        self.f = GRSpec(env_vars="x", sys_vars="y",
+                        env_init="x", env_prog="x",
+                        sys_init="y",
+                        sys_prog=["y & x", "!y"],
+                        moore=False, plus_one=False, )
+        self.dcounter = GRSpec(sys_vars={"y": (0,5)}, sys_init=["y=0"],
+                               sys_prog=["y=0", "y=5"],
+                               moore=False, plus_one=False, qinit='\A \E')
+
+    def tearDown(self):
+        self.f_un = None
+        self.f = None
+        self.dcounter = None
+
+    def test_check_realizable(self):
+        assert not self.check_realizable(self.f_un)
+        self.f_un.sys_safety = []
+        assert self.check_realizable(self.f_un)
+        assert self.check_realizable(self.dcounter)
+
+    def test_synthesize(self):
+        g = self.synthesize(self.f_un)
+        assert not isinstance(g, nx.DiGraph)
+
+        g = self.synthesize(self.f)
+        # There is more than one possible strategy realizing this
+        # specification.  Checking only for one here makes this more like
+        # a regression test (fragile).  However, it is more meaningful
+        # than simply checking that synthesize() returns something
+        # non-None (i.e., realizability, which is tested elsewhere).
+        assert g is not None
+
+        # assert len(g.env_vars) == 1 and g.env_vars.has_key('x')
+        # assert len(g.sys_vars) == 1 and g.sys_vars.has_key('y')
+        print(g.nodes())
+        assert len(g) == 5
+        assert set(g.edges()) == set([(0, 1), (0, 2), (1, 3), (1, 4),
+                                              (2, 3), (2, 4), (3, 0), (3, 3),
+                                              (4, 0), (4, 3)])
+        label_reference = {0: (1,1), # value is bitvector of x,y
+                           1: (0,0),
+                           2: (1,0),
+                           3: (0,0),
+                           4: (1,0)}
+        for u, d in g.nodes_iter(data=True):
+            state = d['state']
+            assert(len(state) == 2)
+            assert(label_reference[u] == (state['x'], state['y']))
