6133f76f41481b5ea898561ba3fa7ea8a6383326
Ioannis Filippidis <jfilippidis@gmail.com>
2017-03-06 02:50:25 -0800

    DOC: link to jtlv website from docstring of module `spec.translation`

--- tulip/spec/translation.py
+++ tulip/spec/translation.py
@@ -33,7 +33,7 @@
 """AST subclasses to translate to each syntax of:
 
   - gr1c: https://tulip-control.github.io/gr1c/md_spc_format.html
-  - JTLV
+  - JTLV: http://jtlv.ysaar.net/
   - SMV: http://nusmv.fbk.eu/NuSMV/userman/v21/nusmv_3.html
   - SPIN: http://spinroot.com/spin/Man/ltl.html
           http://spinroot.com/spin/Man/operators.html
