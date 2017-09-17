c65c91161e362fd3b9de9b31de54ddce184ec359
Ioannis Filippidis <jfilippidis@gmail.com>
2017-09-16 19:24:02 -0700

    PEP8: whitespace

--- contrib/matlabsim/tomatlab.py
+++ contrib/matlabsim/tomatlab.py
@@ -6,9 +6,10 @@ import polytope
 import scipy.io
 import numpy
 
-def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
-    disc_params=None, R=None, r=None, Q=None, mid_weight=None):
 
+def export(
+        filename, mealy_machine, system_dynamics=None, abstraction=None,
+        disc_params=None, R=None, r=None, Q=None, mid_weight=None):
     """Creates two matlab files. One is a script that generates a Simulink model
     that contains a Stateflow Chart of the Mealy Machine, a block that contains
     a get_input function, a block that maps continuous state to discrete state,
@@ -20,7 +21,6 @@ def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
     @param filename: String containing name of the .mat file to be created.
     @rtype: None
     """
-
     # Check whether we're discrete or continuous
     if ((system_dynamics is None) and (abstraction is None) and
         (disc_params is None)):
@@ -32,14 +32,11 @@ def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
         raise StandardError('Cannot tell whether system is continuous or ' +
             'discrete. Please specify dynamics and abstraciton and ' +
             'discretization parameters or none at all.')
-
     output = dict()
     output['is_continuous'] = is_continuous
-
     # Only export dynamics and abstraction and control weights if the system is
     # continuous
     if is_continuous:
-
         # Export system dynamics, get state and input dimension
         if isinstance(system_dynamics, hybrid.LtiSysDyn):
             dynamics_output = lti_export(system_dynamics)
@@ -54,7 +51,6 @@ def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
         elif isinstance(system_dynamics, hybrid.SwitchedSysDyn):
             dynamics_output = switched_export(system_dynamics)
             dynamics_output['type'] = 'SwitchedSysDyn'
-
             # getting state and input dimension by looking at size of the A and
             # B matrices of one of the PWA subsystems
             pwa_systems = list(system_dynamics.dynamics.values())
@@ -65,7 +61,6 @@ def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
             raise TypeError(str(type(system_dynamics)) +
                 ' is not a supported type of system dynamics.')
         output['system_dynamics'] = dynamics_output
-
         # Control weights. Set default values if needed.
         if R is None:
             R = numpy.zeros([state_dimension, state_dimension])
@@ -75,14 +70,12 @@ def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
             Q = numpy.eye(input_dimension)
         if mid_weight is None:
             mid_weight = 3
-
         control_weights = {}
         control_weights['state_weight'] = R
         control_weights['input_weight'] = Q
         control_weights['linear_weight'] = r
         control_weights['mid_weight'] = mid_weight
         output['control_weights'] = control_weights
-
         # Simulation parameters; insert default discretization values if needed
         sim_params = dict()
         try:
@@ -102,22 +95,16 @@ def export(filename, mealy_machine, system_dynamics=None, abstraction=None,
                 raise ValueError('MATLAB interface does not suport ' +
                                  'conservative simulation')
         output['simulation_parameters'] = sim_params
-
         # Abstraction
         output['abstraction'] = export_locations(abstraction)
-
-
     # Export the simulink model
     output['TS'] = export_mealy(mealy_machine, is_continuous)
-
     # Save file
     scipy.io.savemat(filename, output, oned_as='column')
 
 
-
 def lti_export(ltisys):
     """Saves a LtiSysDyn as a Matlab struct."""
-
     output = {}
     output['A'] = ltisys.A
     output['B'] = ltisys.B
@@ -126,28 +113,23 @@ def lti_export(ltisys):
     output['domain'] = poly_export(ltisys.domain)
     output['Uset'] = poly_export(ltisys.Uset)
     output['Wset'] = poly_export(ltisys.Wset)
-
     return output
 
 
 def pwa_export(pwasys):
-
     output = dict()
     output['domain'] = poly_export(pwasys.domain)
     ltisystems = list()
     for subsystem in pwasys.list_subsys:
         ltisystems.append(lti_export(subsystem))
     output['subsystems'] = ltisystems
-
     return output
 
 
 def switched_export(switchedsys):
-
     output = dict()
     output['disc_domain_size'] = list(switchedsys.disc_domain_size)
     output['cts_ss'] = poly_export(switchedsys.cts_ss)
-
     dynamics = list()
     for label, system in switchedsys.dynamics.items():
         system_dict = dict()
@@ -155,9 +137,7 @@ def switched_export(switchedsys):
         system_dict['sys_act'] = label[1]
         system_dict['pwasys'] = pwa_export(system)
         dynamics.append(system_dict)
-
     output['dynamics'] = dynamics
-
     return output
 
 
@@ -167,7 +147,6 @@ def poly_export(poly):
     @param poly: L{Polytope} that will be exported.
     @return output: dictionary containing fields of poly
     """
-
     output = dict()
     if poly is not None:
         output['A'] = poly.A
@@ -194,8 +173,6 @@ def export_locations(abstraction):
 
     @type abstraction: L{AbstractPwa} or L{AbstractSwitched}
     @rtype output: dictionary"""
-
-
     output = dict()
     location_list = list()
     for index, region in enumerate(abstraction.ppp.regions):
@@ -204,17 +181,15 @@ def export_locations(abstraction):
         location_dict['region'] = reg_dict
         location_dict['index'] = index
         location_list.append(location_dict)
-
     output['abstraction'] = location_list
-
     return output
 
 
 def export_mealy_io(variables, values):
     """
+
     @rtype: list of dict
     """
-
     var_list = list()
     for ind, variable in enumerate(variables):
         var_dict = dict()
@@ -234,27 +209,20 @@ def export_mealy(mealy_machine, is_continuous):
 
     @rtype: dict
     """
-
-
     output = dict()
     # States will be exported as a list of dictionaries
     state_list = list()
     for state_tuple in mealy_machine.states.find():
-
         # Do not export Sinit state
         if state_tuple[0] == 'Sinit':
             continue
-
         state_dict = dict()
         state_dict['name'] = state_tuple[0]
-
         # For a continuous system, export the 'loc' variable
         if is_continuous:
             state_dict['loc'] = state_tuple[1]['loc']
-
         state_list.append(state_dict)
     output['states'] = state_list
-
     # Get list of environment and system variables
     env_vars = mealy_machine.inputs.keys()
     env_values = list(mealy_machine.inputs.values())
@@ -264,7 +232,6 @@ def export_mealy(mealy_machine, is_continuous):
     #output['outputs'] = sys_vars
     output['inputs'] = export_mealy_io(env_vars, env_values)
     output['outputs'] = export_mealy_io(sys_vars, sys_values)
-
     # Transitions will be exported as a 2D list of dictionaries. The only
     # purpose of this block here is to separate the inputs from the outputs to
     # make the MATLAB code easier to write
@@ -273,12 +240,11 @@ def export_mealy(mealy_machine, is_continuous):
         # Ignore transitions from Sinit
         if transition_tuple[0] == 'Sinit':
             continue
-
         transition_vals = transition_tuple[2]
-        transition_inputs = { var: str(transition_vals[var])
-                              for var in env_vars }
-        transition_outputs = { var: str(transition_vals[var])
-                               for var in sys_vars }
+        transition_inputs = {var: str(transition_vals[var])
+                             for var in env_vars}
+        transition_outputs = {var: str(transition_vals[var])
+                              for var in sys_vars}
         transition_dict = dict()
         transition_dict['start_state'] = transition_tuple[0]
         transition_dict['end_state'] = transition_tuple[1]
@@ -286,8 +252,6 @@ def export_mealy(mealy_machine, is_continuous):
         transition_dict['outputs'] = transition_outputs
         transitions.append(transition_dict)
     output['transitions'] = transitions
-
-
     # Initial states are the states that have transitions from Sinit. Initial
     # transitions (for the purposes of execution in Stateflow), are the
     # transitions coming from the states that transition from Sinit.
@@ -299,16 +263,13 @@ def export_mealy(mealy_machine, is_continuous):
         from_states=initial_states)
     initial_trans = list()
     for init_transition in initial_transitions:
-
         transition_vals = init_transition[2]
-
         trans_dict = dict()
         trans_dict['state'] = init_transition[1]
-        trans_dict['inputs'] = { var: str(transition_vals[var])
-                                 for var in env_vars }
-        trans_dict['outputs'] = { var: str(transition_vals[var])
-                                  for var in sys_vars }
-
+        trans_dict['inputs'] = {var: str(transition_vals[var])
+                                for var in env_vars}
+        trans_dict['outputs'] = {var: str(transition_vals[var])
+                                 for var in sys_vars}
         orig_state = init_transition[0]
         orig_loc = initial_states[orig_state]
         trans_dict['start_loc'] =  orig_loc
