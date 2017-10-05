a8b07175f3f5777b6bdf1243a7de64462d94ff33
Ioannis Filippidis <jfilippidis@gmail.com>
2017-10-04 20:18:54 -0700

    STY: `autopep8 -i tulip-in-a-box.py`

--- contrib/tulip-in-a-box.py
+++ contrib/tulip-in-a-box.py
@@ -83,14 +83,14 @@ with open(travis_yml_path) as fp:
 # Arrange base environment
 subprocess.check_call(
     (sudo_prefix +
-    'apt-get -y install python-pip libpython-dev ' +
-    'libpython3-dev python-virtualenv'
-    ).split())
+     'apt-get -y install python-pip libpython-dev ' +
+     'libpython3-dev python-virtualenv'
+     ).split())
 subprocess.check_call((sudo_prefix + 'pip install -I -U pip').split())
 subprocess.check_call(
     (sudo_prefix + 'apt-get -y install ' +
-    ' '.join(travis_config['addons']['apt']['packages'])
-    ).split())
+     ' '.join(travis_config['addons']['apt']['packages'])
+     ).split())
 subprocess.check_call(['virtualenv', '-p', 'python3', 'PYt'])
 
 # Main script
