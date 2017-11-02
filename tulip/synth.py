tulip-control/tulip-control

commit 74807640a1a1fe6da2e579189dc5ce4a0b0ea118

fix imports, arguments and return values of moved function

tulip/synth.py
@@ -38,6 +38,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 import warnings
+from collections import OrderedDict
 
 from . import transys
 from .spec import GRSpec
@@ -1243,7 +1244,8 @@ def synthesize(
                           bool_actions)
     
     if option == 'gr1c':
-        ctrl = gr1c.synthesize(specs)
+        strategy = gr1c.synthesize(specs)
+        ctrl = strategy2mealy(strategy, specs)
     elif option == 'jtlv':
         ctrl = jtlv.synthesize(specs)
     else:
@@ -1324,25 +1326,32 @@ def spec_plus_sys(
     logger.info('Overall Spec:\n' + str(specs.pretty() ) +_hl)
     return specs
 
-def strategy2mealy(A, env_vars, sys_vars, spec, spec0):
-    """
+def strategy2mealy(A, spec0):
+    """Convert strategy to Mealy transducer.
+    
+    Note that the strategy is a deterministic game graph,
+    but the input C{A} is given as the contraction of
+    this game graph (which directly )
+    
+    @param A: strategy
+    @type A: C{networkx.DiGraph}
     
     @return: tuple of the form (L{GRSpec}, L{MealyMachine}).  Either
         or both can be None if the corresponding part is missing.
         Note that the returned GRSpec instance depends only on what is
         in the given tulipcon XML string x, not on the argument spec0.
     """
-    if spec0 is None:
-        spec0 = spec
+    env_vars = spec0.env_vars
+    sys_vars = spec0.sys_vars
     
     # show port only when true (or non-zero for int-valued vars)
     mask_func = bool
     
-    mach = MealyMachine()
-    inputs = create_machine_ports(spec0.env_vars)
+    mach = transys.MealyMachine()
+    inputs = transys.machines.create_machine_ports(spec0.env_vars)
     mach.add_inputs(inputs)
     
-    outputs = create_machine_ports(spec0.sys_vars)
+    outputs = transys.machines.create_machine_ports(spec0.sys_vars)
     masks = {k:mask_func for k in sys_vars}
     mach.add_outputs(outputs, masks)
     
@@ -1424,7 +1433,7 @@ def strategy2mealy(A, env_vars, sys_vars, spec, spec0):
             label = _map_int2dom(var_values, arbitrary_domains)
             mach.transitions.add(initial_state, node, **label)
     
-    return (spec, mach)
+    return mach
 
 def _map_int2dom(label, arbitrary_domains):
     """For custom finite domains map int values to domain elements.

tulip/interfaces/gr1c.py
@@ -46,14 +46,11 @@ logger = logging.getLogger(__name__)
 
 import copy
 import subprocess
-from collections import OrderedDict
 import tempfile
 import xml.etree.ElementTree as ET
 import networkx as nx
 
-from tulip.transys.machines import create_machine_ports
 from tulip.spec import GRSpec
-from tulip.transys import MealyMachine
 
 GR1C_BIN_PREFIX=""
 _hl = '\n' +60*'-'
@@ -406,8 +403,8 @@ def synthesize(spec, init_option="ALL_ENV_EXIST_SYS_INIT"):
     logger.debug('gr1c stdout, stderr:\n' + str(stdoutdata) +_hl)
     
     if p.returncode == 0:
-        (spec, aut) = aut_xml2mealy(stdoutdata, spec0=spec)
-        return aut
+        strategy = aut_xml2mealy(stdoutdata)
+        return strategy
     else:
         print(30*' ' + '\n gr1c return code:\n' + 30*' ')
         print(p.returncode)
@@ -425,9 +422,9 @@ def load_mealy(filename):
     @rtype: L{MealyMachine}
     """
     s = open(filename, 'r').read()
-    spec, aut = aut_xml2mealy(s)
-    logger.info('Loaded spec: \n' + spec.pretty() )
-    return aut
+    strategy = aut_xml2mealy(s)
+    #logger.info('Loaded spec: \n' + spec.pretty() )
+    return strategy
 
 class GR1CSession:
     """Manage interactive session with gr1c.

commit f6f528be803ebf3d69c9bcd5b376778a8056472e

BUG: convert strategy to `MealyMachine` in `synthesize_many`
The error here was a missing conversion from an enumerated
state graph with the synthesized transitions to a Mealy machine.
This error appears to have been introduced in
74807640a1a1fe6da2e579189dc5ce4a0b0ea118.

The conversion was previously within the module
`tulip.interfaces.gr1c`. When the conversion was moved to
`tulip.synth`, the function `synthesize` was updated,
but the function `synthesize_many` was forgotten.
This is how the error was introduced.

tulip/synth.py
@@ -991,30 +991,19 @@ def synthesize_many(specs, ts=None, ignore_init=None,
             _copy_options_from_ts(env_spec, t, specs)
             specs |= env_spec
     if solver == 'gr1c':
-        ctrl = gr1c.synthesize(specs)
+        strategy = gr1c.synthesize(specs)
     elif solver == 'slugs':
         if slugs is None:
             raise ValueError(
                 'Import of slugs interface failed. '
                 'Please verify installation of "slugs".')
-        ctrl = slugs.synthesize(specs)
+        strategy = slugs.synthesize(specs)
     else:
         raise Exception((
             'Unknown solver: "{solver}". '
             'Available solvers: "gr1c", and "slugs"').format(
                 solver=solver))
-    try:
-        logger.debug(
-            'Mealy machine has: n = {n} states.'.format(
-                n=len(ctrl.states)))
-    except:
-        logger.debug('No Mealy machine returned.')
-    # no controller found ?
-    # counterstrategy not constructed by synthesize
-    if not isinstance(ctrl, transys.MealyMachine):
-        return None
-    ctrl.remove_deadends()
-    return ctrl
+    return _trim_strategy(strategy, rm_deadends=True)
 
 
 def synthesize(