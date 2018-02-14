bfa2de54e841cacd10b5fcfa53880a0d80fb5aed
zexiangliu <zexiang@umich.edu>
2018-02-14 16:45:05 -0500

    add output subfun for the simu_abstract;add test code

---
+++ tests/transys_simu_abstract_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Tests for transys.transys.simu_abstract (part of transys subpackage)
+"""
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+from tulip import transys as trs
+
+
+def simu_abstract_test():
+    
--- tulip/transys/transys.py
+++ tulip/transys/transys.py
@@ -796,7 +796,7 @@ def simu_abstract(ts,simu_type):
     
     @param ts: L{FTS}
     @param simu_type: string 'bi'/'dual', flag used to switch b.w. bisimu and dual-simu
-    @return: L{FTS}, C{networkx.MultiDiGraph}
+    @return: the bi/dual simulation FTS L{FTS}, and the corresponding partition
     
     References
     ==========
@@ -805,7 +805,7 @@ def simu_abstract(ts,simu_type):
     54th Annual Allerton Conference on CCC 2016, 569–576.
     """
     def pre(graph,list_n):
-        """Predecessor
+        """Predecessor of a set of node in original FTS
         
         @param graph: graph defined in C{MultiDiGraph}
         @param list_n: list of nodes
@@ -816,20 +816,66 @@ def simu_abstract(ts,simu_type):
         for n in list_n:
             pre_set = pre_set.union(graph.predecessors(n))
         return pre_set
+    def output_FTS(ts,Part):
+        """ Convert the Part from nx.MultiDiGraph to FTS
+        
+        @param ts: original FTS L{FTS}
+        @param Part: the final partition
+        
+        @return:  the bi/dual simulation FTS L{FTS}, and the corresponding partition
+        """
+        env_actions = [
+            {'name': 'env_actions',
+             'values': ts.env_actions,
+             'setter': True}]
+        sys_actions = [
+                    {'name': 'sys_actions',
+                     'values': ts.sys_actions,
+                     'setter': True}]
+        ts_simu = FTS(env_actions,sys_actions)
+        ts2simu = {}
+        simu2ts = {}
+        Part_hash = {'ts2simu':ts2simu,'simu2ts':simu2ts}
+        for i in Part:
+            simu2ts[i] = Part.node[i]['cov']
+            for j in Part.node[i]['cov']:
+                if(ts2simu.has_key(j)):
+                    ts2simu[j].append(i)
+                else:
+                    ts2simu[j]=[i]
+        
+        S = Part.nodes()
+        S0 = set()
+        for i in ts.states.initial:
+            [S0.add(j) for j in ts2simu[i]]
+        AP = ts.aps
+        
+        L = []
+        for i in Part:
+            L.append((i,eval(Part.node[i]['ap'])))
     
+        Act = None
+s
+        trans=[]
+        
+        for i,j in Part.edges_iter():
+            trans.append((i,j))    
+            
+        ts_simu = tuple2fts(S,S0,AP,L,Act,trans,name=simu_type,prepend_str='')
+        return ts_simu, Part_hash
     # recover the FTS to MultiDiGraph
     G = MultiDiGraph(ts)
     
     # build coarsest partition (graph + hash table)
     S0 = dict()
-    G0 = nx.MultiDiGraph() # a graph associated with the new partition
+    Part = nx.MultiDiGraph() # a graph associated with the new partition
     num_cell = 0
     
     for node in G:
         ap = repr(G.node[node]['ap'])
         if not S0.has_key(ap):
             S0[ap]=set()
-            G0.add_node(num_cell,ap=ap,cov=S0[ap]) # hash table S0--->G
+            Part.add_node(num_cell,ap=ap,cov=S0[ap]) # hash table S0--->G
             num_cell += 1
             
         S0[ap].add(node)
@@ -837,12 +883,12 @@ def simu_abstract(ts,simu_type):
     # build a queue of node, used for the while loop
     queue = Queue()
     # add edges in the coarsest partition, and add nodes in the queue
-    for i in G0:
-        pre_i = pre(G,G0.node[i]['cov'])    
-        for j in G0:
-            cov_j = G0.node[j]['cov']
+    for i in Part:
+        pre_i = pre(G,Part.node[i]['cov'])    
+        for j in Part:
+            cov_j = Part.node[j]['cov']
             if pre_i.intersection(cov_j)!=set():
-                G0.add_edge(j,i)
+                Part.add_edge(j,i)
                 
         queue.put(i)
            
@@ -851,27 +897,27 @@ def simu_abstract(ts,simu_type):
         # pop a node from the queue
         i = queue.get()
         # calculuate its pre in G
-        pre_i = pre(G,G0.node[i]['cov'])
+        pre_i = pre(G,Part.node[i]['cov'])
         # intersect the pre of node with states of other nodes
-        for j in G0.predecessors(i):
-            cov_j = G0.node[j]['cov']
+        for j in Part.predecessors(i):
+            cov_j = Part.node[j]['cov']
             
             if pre_i.intersection(cov_j)==set():
-                G0.remove_edge(j,i)
+                Part.remove_edge(j,i)
                 continue
             
             if not cov_j.issubset(pre_i):
                 #  add new node and update the graph 
-                G0.node[j]['cov'] = cov_j.intersection(pre_i)
+                Part.node[j]['cov'] = cov_j.intersection(pre_i)
                 if(simu_type=='dual'):
-                    G0.add_node(num_cell,ap=G0.node[j]['ap'],cov=cov_j)
+                    Part.add_node(num_cell,ap=Part.node[j]['ap'],cov=cov_j)
                 elif (simu_type=='bi'):
-                    G0.add_node(num_cell,ap=G0.node[j]['ap'],cov=cov_j.difference(pre_i))
+                    Part.add_node(num_cell,ap=Part.node[j]['ap'],cov=cov_j.difference(pre_i))
                                   
-                [G0.add_edge(k,num_cell) for k in G0.predecessors_iter(j)]
-                [G0.add_edge(num_cell,k) for k in G0.successors_iter(j)]
-#                G0.add_edges_from([(j,num_cell),(num_cell,j)])  
-                G0.remove_edge(num_cell,i) # remove wrong edge in coarser part
+                [Part.add_edge(k,num_cell) for k in Part.predecessors_iter(j)]
+                [Part.add_edge(num_cell,k) for k in Part.successors_iter(j)]
+#                Part.add_edges_from([(j,num_cell),(num_cell,j)])  
+                Part.remove_edge(num_cell,i) # remove wrong edge in coarser part
                 # add effected cells into the queue
                 queue.put(j)
                 queue.put(num_cell)
@@ -882,54 +928,6 @@ def simu_abstract(ts,simu_type):
 #        print 'num of cell is', num_cell
                     
     # construct new FTS
-    env_actions = [
-                {'name': 'env_actions',
-                 'values': ts.env_actions,
-                 'setter': True}]
-    sys_actions = [
-                {'name': 'sys_actions',
-                 'values': ts.sys_actions,
-                 'setter': True}]
-    ts_simu = FTS(env_actions,sys_actions)
-    ts2simu = {}
-    for i in G0:
-        for j in G0.node[i]['cov']:
-            if(ts2simu.has_key(j)):
-                ts2simu[j].append(i)
-            else:
-                ts2simu[j]=[i]
-            
-    S = G0.nodes()
-    S0 = set()
-    for i in ts.states.initial:
-        [S0.add(j) for j in ts2simu[i]]
-    AP = ts.aps
+    [ts_simu, Part_hash] =output_FTS(ts,Part)
     
-    L = []
-    for i in G0:
-        L.append((i,eval(G0.node[i]['ap'])))
-    # do we consider actions for bi/dual simulation abstraction?
-#    if((len(ts.actions['sys_actions'])+len(ts.actions['env_actions']))==0):
-    Act = None
-#    else:
-#        Act = ts.actions
-    
-    trans=[]
-#    if((len(ts.actions['sys_actions'])+len(ts.actions['env_actions']))!=0):
-#        for i,j in G0.edges_iter():
-#            trans.append((i,j,G0.edge[i][j]))
-#    else:
-    for i,j in G0.edges_iter():
-        trans.append((i,j))    
-        
-    ts_simu = tuple2fts(S,S0,AP,L,Act,trans,name=simu_type,prepend_str='')
-    
-#    ts_simu.add_nodes_from(G0.nodes())
-#    ts_simu.states.initial.add(S0)
-#    ts_simu.add_edges_from(G0.edges())
-#    ts_simu.atomic_propositions.add_from(ts.atomic_propositions)
-#    for i in ts_simu:
-#        ts_simu.states.add(i, ap=G0.node[i]['ap'])
-    # build hash-table mapping nodes in ts to nodes in G0
-    
-    return ts_simu, G0
+    return ts_simu, Part_hash
