6b399a2ea1a2ffd95077fd224fed6292dd25221d
Scott C. Livingston <slivingston@cds.caltech.edu>
2019-12-20 19:41:56 -0800

    CI: new location for Lily
    the old URL,
    https://www.iaik.tugraz.at/content/research/opensource/lily/lily-2008-06-19.tar.gz
    is not valid now.
    
    we should contact the original authors eventually, but for now I
    posted a copy of the tarball to the cudd-mirror SourceForge project
    (https://sourceforge.net/projects/cudd-mirror/files/) that I created
    when the CUDD website was removed. these are all open source, so
    hosting mirrors is OK, and the SHA hashes provide certificates that
    the data are as on their original websites.

--- extern/get-lily.sh
+++ extern/get-lily.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-curl -ksO https://www.iaik.tugraz.at/content/research/opensource/lily/lily-2008-06-19.tar.gz -o lily-2008-06-19.tar.gz
+curl -sSL https://sourceforge.net/projects/cudd-mirror/files/lily-2008-06-19.tar.gz/download -o lily-2008-06-19.tar.gz
 echo '2442e15223e116a4ebf6fbab42646ee64111f286458b4393f18ac7b1a9e19c952bba91f548e0e9c323d50f29e639a5089bf117c5e7eae874f1e9c677f3c44aee  lily-2008-06-19.tar.gz' | \
 shasum -a 512 -c -
 tar xzf lily-2008-06-19.tar.gz
