bb004422b575dccea8d19c33acfeb04b37c62a5a
Ioannis Filippidis <jfilippidis@gmail.com>
2020-11-24 17:23:52 +0200

    REL: add Trove classifier for Python 3.9

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ classifiers = [
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Topic :: Scientific/Engineering']
 package_data = {
     'tulip.spec': ['parsetab.py']}
