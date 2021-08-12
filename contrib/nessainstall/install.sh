f29993141b269d6d43c8154aec698c8147c001d7
Ioannis Filippidis <jfilippidis@gmail.com>
2021-08-12 22:05:01 +0200

    STY: list one `apt` package per line, within comment

--- contrib/nessainstall/install.sh
+++ contrib/nessainstall/install.sh
@@ -18,9 +18,19 @@
 # debian/ubuntu dependencies:
 #
 # sudo apt-get install \
-#   build-essential libreadline-gplv2-dev libncursesw5-dev \
-#   libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev \
-#   libbz2-dev libatlas-base-dev libatlas-dev gfortran libpng-dev
+#   build-essential \
+#   libreadline-gplv2-dev \
+#   libncursesw5-dev \
+#   libssl-dev \
+#   libsqlite3-dev \
+#   tk-dev \
+#   libgdbm-dev \
+#   libc6-dev \
+#   libbz2-dev \
+#   libatlas-base-dev \
+#   libatlas-dev \
+#   gfortran \
+#   libpng-dev
 
 # e.g.: ~/.bashrc if using bash,
 # or:   ~/.tcshrc if using csh
