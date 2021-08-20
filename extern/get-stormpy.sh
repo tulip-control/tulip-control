b38b525aca9d918b4737c10a1e02625cba4a49fd
Ioannis Filippidis <jfilippidis@gmail.com>
2021-08-20 17:34:01 +0200

    REL: download updated binaries from GitHub in `get-stormpy.sh`
    - MNT: binaries built on Ubuntu 20.04.2
    - DOC: list `apt` packages used for
      building the binaries
    - API: internal arrangement of tarballs changed:
      The directory tree that results from unpacking
      the tarball is now part of the directory tree
      that results from executing the build commands)
    - API: where the tarballs are extracted to has changed
    - paths `carl_DIR` and `storm_DIR` changed,
      following the changes to the tarballs,
      and to where the tarballs are extracted to.

--- extern/get-stormpy.sh
+++ extern/get-stormpy.sh
@@ -1,21 +1,54 @@
 #!/bin/bash -e
 #
 # dependency support script for stormpy interface of TuLiP
+#
+# The following `apt` packages are needed for
+# building the binaries for `carl` and `storm`
+# by following the commands listed below:
+#
+# autoconf
+# build-essential
+# cmake
+# doxygen
+# git
+# libboost-all-dev
+# libeigen3-dev
+# libginac-dev
+# libtool
+# python2
+# texinfo
+# z3
+# libz3-dev
+# python3-z3
+#
+#
 # initially created 2020-10-05
 
 ## CArL https://smtrat.github.io/carl/
-if [ ! -f carl.tar.xz ]
-then
-    curl -L -sS -o carl.tar.xz https://sourceforge.net/projects/cudd-mirror/files/carl.tar.xz/download
-fi
-echo 'cdcc8726aca2d1d1486c5746b5f4b729de2267aaf1dedfa72db9c835e67daa36  carl.tar.xz' | \
+# if [ ! -f carl.tar.xz ]
+# then
+#     curl -L -sS -o carl.tar.xz https://sourceforge.net/projects/cudd-mirror/files/carl.tar.xz/download
+# fi
+# echo 'cdcc8726aca2d1d1486c5746b5f4b729de2267aaf1dedfa72db9c835e67daa36  carl.tar.xz' | \
+#     shasum -a 256 -c -
+# xz -d carl.tar.xz && tar xf carl.tar
+# export PREPLACE=$(echo $PWD | tail -c +2)
+# export PREPLACE=$(echo $PREPLACE | sed 's/\//\\\//g')
+# sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" carl/carlConfig.cmake
+# sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" carl/carlTargets.cmake
+# export carl_DIR=${PWD}/carl
+
+curl -L -sS -o carl.tar.gz \
+https://github.com/tulip-control/data/releases/download/stormpy_dependencies/carl.tar.gz
+echo 'abe2e0df679fc18322986e9609ffdd2fa36300d240c8a37510b1b9902e037502  carl.tar.gz' | \
     shasum -a 256 -c -
-xz -d carl.tar.xz && tar xf carl.tar
+mkdir -p extern
+tar -xzf carl.tar.gz -C extern
 export PREPLACE=$(echo $PWD | tail -c +2)
 export PREPLACE=$(echo $PREPLACE | sed 's/\//\\\//g')
-sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" carl/carlConfig.cmake
-sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" carl/carlTargets.cmake
-export carl_DIR=${PWD}/carl
+sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" extern/carl/build/carlConfig.cmake
+sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" extern/carl/build/carlTargets.cmake
+export carl_DIR=${PWD}/extern/carl/build
 
 
 ## to build CArL from source:
@@ -26,6 +59,23 @@ export carl_DIR=${PWD}/carl
 # cmake -DUSE_CLN_NUMBERS=ON -DUSE_GINAC=ON -DTHREAD_SAFE=ON ..
 # make lib_carl
 # popd
+
+# to create the tarball that is uploaded to GitHub:
+#
+# Edit the files:
+#   carl/build/carlConfig.cmake
+#   carl/build/carlTargets.cmake
+# to replace the paths specific to the machine
+# with the string `PREFIXTOREPLACE`, to be used
+# by `sed` as above.
+# Then:
+#
+# tar -czf carl.tar.gz \
+#     carl/src \
+#     carl/build/carlConfig.cmake \
+#     carl/build/carlTargets.cmake \
+#     carl/build/libcarl.so.14.20 \
+#     carl/build/src
 
 
 ## pycarl https://github.com/moves-rwth/pycarl/
@@ -42,16 +92,26 @@ popd
 
 
 ## Storm https://www.stormchecker.org/
-if [ ! -f storm.tar.xz ]
-then
-    curl -L -sS -o storm.tar.xz https://sourceforge.net/projects/cudd-mirror/files/storm.tar.xz/download
-fi
-echo '25ce93853da67162922e49c37cccba9b2fcb73ac11facbab93b26690957ddc7d  storm.tar.xz' | \
+# if [ ! -f storm.tar.xz ]
+# then
+#     curl -L -sS -o storm.tar.xz https://sourceforge.net/projects/cudd-mirror/files/storm.tar.xz/download
+# fi
+# echo '25ce93853da67162922e49c37cccba9b2fcb73ac11facbab93b26690957ddc7d  storm.tar.xz' | \
+#     shasum -a 256 -c -
+# xz -d storm.tar.xz && tar xf storm.tar
+# sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" storm/stormConfig.cmake
+# sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" storm/stormTargets.cmake
+# export storm_DIR=${PWD}/storm
+
+curl -L -sS -o storm.tar.gz \
+https://github.com/tulip-control/data/releases/download/stormpy_dependencies/storm.tar.gz
+echo 'ff983436bc572f80b62e5dabc849376d25c0e69c0819435bc5ae238e927aaac5  storm.tar.gz' | \
     shasum -a 256 -c -
-xz -d storm.tar.xz && tar xf storm.tar
-sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" storm/stormConfig.cmake
-sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" storm/stormTargets.cmake
-export storm_DIR=${PWD}/storm
+mkdir -p extern
+tar -xzf storm.tar.gz -C extern
+sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" extern/storm/build/stormConfig.cmake
+sed -i "s/PREFIXTOREPLACE/$PREPLACE/g" extern/storm/build/stormTargets.cmake
+export storm_DIR=${PWD}/extern/storm/build
 
 
 ## to build Storm from source:
@@ -65,6 +125,31 @@ export storm_DIR=${PWD}/storm
 # cd bin
 # export PATH=`pwd`:$PATH
 # popd
+
+# to create the tarball that is uploaded to GitHub:
+#
+# Edit:
+#   storm/build/stormConfig.cmake
+#   storm/build/stormTargets.cmake
+# to replace the paths specific to the machine
+# with the string `PREFIXTOREPLACE`, to be used
+# by `sed` as above.
+# Then:
+#
+#
+# tar -czf storm.tar.gz \
+#     storm/resources/3rdparty/cpptemplate \
+#     storm/resources/3rdparty/exprtk \
+#     storm/resources/3rdparty/gmm-5.2/include \
+#     storm/resources/3rdparty/l3pp \
+#     storm/resources/3rdparty/modernjson/src \
+#     storm/resources/3rdparty/sylvan/src \
+#     storm/build/stormConfig.cmake \
+#     storm/build/stormTargets.cmake \
+#     storm/build/bin \
+#     storm/build/lib \
+#     storm/build/include \
+#     storm/build/resources
 
 
 ## stormpy https://moves-rwth.github.io/stormpy/
