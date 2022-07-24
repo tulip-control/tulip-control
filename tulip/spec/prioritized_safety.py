1f29e02a076e1af687c97fa43c9da61145cac5d8
Ioannis Filippidis <jfilippidis@gmail.com>
2022-07-25 00:28:27 +0200

    DOC: add docstring to module `tulip.spec.prioritized_safety`

--- tulip/spec/prioritized_safety.py
+++ tulip/spec/prioritized_safety.py
@@ -29,7 +29,8 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
-
+#
+"""Representing prioritized safety specifications."""
 from itertools import product
 
 from tulip.transys.automata import FiniteStateAutomaton as FA
