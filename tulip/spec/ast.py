7537aa7099201f87ab6a33011015e15f590aeb1f
Ioannis Filippidis <jfilippidis@gmail.com>
2022-07-25 15:10:01 +0200

    REF: move class definitions to module scope, for AST node functors
    The AST nodes are parametrized on two levels:
    
    1. The `opmap` class-attribute defines a mapping of operators,
       which used almost only for non-nullary operator nodes
       (and also for Boolean constants; class `Bool`).
    
    2. The instance attributes, which define what specific
       node this is, e.g., the node for the value `TRUE`,
       or the operator `+`.
    
    The `opmap` parametrization is currently arranged at
    the class level. So the functions `make_nodes()` and
    `make_fol_nodes()` are class factories. The returned
    value is a class that serves as a namespace that
    contains classes, thus these functions resemble functors.
    
    Flat is better than nested. So most of the
    class definitions (i.e., the methods) is moved by
    this commit to module scope. The module-scope classes
    are now also inheriting from `typing.Protocol`.
    
    The use of `typing.Protocol` aligns with the
    factory-flavor of the module `spec.ast`.
    This approach facilitates type hinting,
    for example the return-type `NodesSpec` for
    the function `tulip.spec.ast.make_nodes`.

--- tulip/spec/ast.py
+++ tulip/spec/ast.py
@@ -37,11 +37,13 @@ Syntax taken originally roughly from:
 """
 import collections.abc as _abc
 import logging
+import typing as _ty
 
 
 logger = logging.getLogger(__name__)
 
 
+OpMap = dict[str, str]
 # prototype for flattening to a "canonical" string
 OPMAP = {
     'False':
@@ -131,7 +133,219 @@ FULL_OPERATOR_NAMES = {
     }
 
 
-def make_nodes(opmap=None):
+class NodeSpec(_ty.Protocol):
+    """Base class for AST nodes."""
+
+    opmap: (
+        OpMap |
+        None
+        ) = None
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        raise NotImplementedError
+
+    def flatten(self):
+        raise NotImplementedError
+
+
+class TerminalSpec(NodeSpec):
+    """Terminal symbols of grammar.
+
+    Include:
+
+    - 0-ary function constants (numbers, strings)
+    - 0-ary function variables (integer or string variable)
+    - 0-ary connectives (Boolean constants)
+    - 0-ary predicate constants
+    - 0-ary predicate variables
+    """
+
+    def __init__(self, value):
+        try:
+            value + 'a'
+        except TypeError:
+            raise TypeError(
+                f'value must be a string, got: {value}')
+        self.type = 'terminal'
+        self.value = value
+
+    def __repr__(self):
+        t = type(self).__name__
+        v = repr(self.value)
+        return f'{t}({v})'
+
+    def __hash__(self):
+        return id(self)
+
+    def __str__(self, *arg, **kw):
+        # *arg accommodates "depth" arg of Operator.__str__
+        return self.value
+
+    def __len__(self):
+        """Return the number of operators and terminals.
+
+        Note that this definition differs from the
+        theoretical definition that a formula's length
+        is the number of operators it contains.
+        """
+        return 1
+
+    def __eq__(self, other):
+        return (isinstance(other, type(self)) and
+                self.value == other.value)
+
+    def flatten(self, *arg, **kw):
+        return self.value
+
+
+class OperatorSpec(NodeSpec):
+    """Takes a non-zero number of operands and returns a result.
+
+    Cases:
+
+      - function (arithmetic):
+          maps (terms)^n to terms
+      - predicate (relational operator):
+          maps (terms)^n to atomic formulas
+      - connective (logical operator):
+          maps (wff)^n to wff
+    """
+
+    def __init__(self, operator, *operands):
+        try:
+            operator + 'a'
+        except TypeError:
+            raise TypeError(
+                f'operator must be string, got: {operator}')
+        self.type = 'operator'
+        self.operator = operator
+        self.operands = list(operands)
+
+    def __repr__(self):
+        t = type(self).__name__
+        op = repr(self.operator)
+        xyz = _comma(map(
+            repr, self.operands))
+        return f'{t}({op}, {xyz})'
+
+    # depth allows limiting recursion to see a shallower view
+    def __str__(self, depth=None):
+        if depth is not None:
+            depth = depth - 1
+        if depth == 0:
+            return '...'
+        op = self.operator
+        def str_of(x):
+            return x.__str__(depth=depth)
+        xyz = ' '.join(map(
+            str_of, self.operands))
+        return f'({op} {xyz})'
+
+    def __len__(self):
+        return 1 + sum(map(len, self.operands))
+
+    def flatten(self, *arg, **kw):
+        def flatten(x):
+            return x.flatten(*arg, **kw)
+        op = self.opmap[self.operator]
+        args = _comma(map(
+            flatten, self.operands))
+        return f'( {op} {args} )'
+
+
+class BinarySpec(OperatorSpec):
+    def flatten(self, *arg, **kw):
+        """Infix flattener for consistency with parser.
+
+        Override it if you want prefix or postfix.
+        """
+        op = self.opmap[self.operator]
+        def flatten(x):
+            return x.flatten(*arg, **kw)
+        first, second = map(flatten, self.operands)
+        return f'( {first} {op} {second} )'
+
+
+class NodesSpec(_ty.Protocol):
+    """AST nodes for a generic grammar."""
+
+    Node: NodeSpec
+    Terminal: TerminalSpec
+    Operator: OperatorSpec
+    Unary: OperatorSpec
+    Binary: BinarySpec
+
+
+class VarSpec(TerminalSpec):
+    """A 0-ary variable.
+
+    Two cases:
+
+      - 0-ary function variable (integer or string variable)
+      - 0-ary propositional variable (atomic proposition)
+    """
+
+    def __init__(self, value):
+        super().__init__(value)
+        self.type = 'var'
+
+
+class BoolSpec(TerminalSpec):
+    """A 0-ary connective."""
+
+    def __init__(self, value):
+        try:
+            value + 'a'
+        except TypeError:
+            raise TypeError(
+                f'value must be string, got: {value}')
+        if value.lower() not in {'true', 'false'}:
+            raise TypeError(
+                'value must be "true" or "false" '
+                f'(case insensitive), got: {value}')
+        self.value = value.title()
+        self.type = 'bool'
+
+    def flatten(self, *arg, **kw):
+        return self.opmap[self.value]
+
+
+class NumSpec(TerminalSpec):
+    """A 0-ary function."""
+
+    def __init__(self, value):
+        super().__init__(value)
+        self.type = 'num'
+
+
+class StrSpec(TerminalSpec):
+    """A 0-ary function."""
+    # parser ensures that value has no quotes
+
+    def __init__(self, value):
+        super().__init__(value)
+        self.type = 'str'
+
+
+class ComparatorSpec(BinarySpec):
+    """Binary relational operator (2-ary predicate)."""
+
+
+class ArithmeticSpec(BinarySpec):
+    """Binary function.
+
+    Maps terms to terms.
+    """
+
+
+def make_nodes(
+        opmap:
+            OpMap |
+            None=None
+        ) -> NodesSpec:
     """Return class with attributes the AST node classes.
 
     The tree is defined recursively,
@@ -139,143 +353,22 @@ def make_nodes(opmap=None):
     `Tree` is a graph data structure for that purpose.
     """
     if opmap is None:
-        opmap = OPMAP
-    class Node:
-        """Base class for AST nodes."""
-        opmap = None
-
-        def __init__(self):
-            raise NotImplementedError
-
-        def __repr__(self):
-            raise NotImplementedError
-
-        def flatten(self):
-            raise NotImplementedError
-    Node.opmap = opmap
-
-    class Terminal(Node):
-        """Terminal symbols of grammar.
-
-        Include:
-
-        - 0-ary function constants (numbers, strings)
-        - 0-ary function variables (integer or string variable)
-        - 0-ary connectives (Boolean constants)
-        - 0-ary predicate constants
-        - 0-ary predicate variables
-        """
-
-        def __init__(self, value):
-            try:
-                value + 'a'
-            except TypeError:
-                raise TypeError(
-                    f'value must be a string, got: {value}')
-            self.type = 'terminal'
-            self.value = value
-
-        def __repr__(self):
-            t = type(self).__name__
-            v = repr(self.value)
-            return f'{t}({v})'
-
-        def __hash__(self):
-            return id(self)
-
-        def __str__(self, *arg, **kw):
-            # *arg accommodates "depth" arg of Operator.__str__
-            return self.value
-
-        def __len__(self):
-            """Return the number of operators and terminals.
-
-            Note that this definition differs from the
-            theoretical definition that a formula's length
-            is the number of operators it contains.
-            """
-            return 1
-
-        def __eq__(self, other):
-            return (isinstance(other, type(self)) and
-                    self.value == other.value)
-
-        def flatten(self, *arg, **kw):
-            return self.value
-
-    class Operator(Node):
-        """Takes a non-zero number of operands and returns a result.
-
-        Cases:
-
-          - function (arithmetic):
-              maps (terms)^n to terms
-          - predicate (relational operator):
-              maps (terms)^n to atomic formulas
-          - connective (logical operator):
-              maps (wff)^n to wff
-        """
-
-        def __init__(self, operator, *operands):
-            try:
-                operator + 'a'
-            except TypeError:
-                raise TypeError(
-                    f'operator must be string, got: {operator}')
-            self.type = 'operator'
-            self.operator = operator
-            self.operands = list(operands)
-
-        def __repr__(self):
-            t = type(self).__name__
-            op = repr(self.operator)
-            xyz = _comma(map(
-                repr, self.operands))
-            return f'{t}({op}, {xyz})'
-
-        # depth allows limiting recursion to see a shallower view
-        def __str__(self, depth=None):
-            if depth is not None:
-                depth = depth - 1
-            if depth == 0:
-                return '...'
-            op = self.operator
-            def str_of(x):
-                return x.__str__(depth=depth)
-            xyz = ' '.join(map(
-                str_of, self.operands))
-            return f'({op} {xyz})'
-
-        def __len__(self):
-            return 1 + sum(map(len, self.operands))
-
-        def flatten(self, *arg, **kw):
-            def flatten(x):
-                return x.flatten(*arg, **kw)
-            op = self.opmap[self.operator]
-            args = _comma(map(
-                flatten, self.operands))
-            return f'( {op} {args} )'
-
-
+        _opmap = OPMAP
+    else:
+        _opmap = opmap
+    class Node(NodeSpec):
+        opmap = _opmap
+    class Terminal(TerminalSpec, Node):
+        pass
+    class Operator(OperatorSpec, Node):
+        pass
     class Unary(Operator):
         pass
-
-    class Binary(Operator):
-        def flatten(self, *arg, **kw):
-            """Infix flattener for consistency with parser.
-
-            Override it if you want prefix or postfix.
-            """
-            op = self.opmap[self.operator]
-            def flatten(arg):
-                return arg.flatten(*arg, **kw)
-            first, second = map(flatten, self.operands)
-            return f'( {first} {op} {second} )'
-
-    class Nodes:
-        """AST nodes for a generic grammar."""
-
+    class Binary(BinarySpec, Operator):
+        pass
+    class Nodes(
+            NodesSpec):
+        pass
     nodes = Nodes()
     nodes.Node = Node
     nodes.Terminal = Terminal
@@ -285,66 +378,25 @@ def make_nodes(opmap=None):
     return nodes
 
 
-def make_fol_nodes(opmap=None):
+def make_fol_nodes(
+        opmap:
+            OpMap |
+            None=None
+        ) -> NodesSpec:
     """AST classes for fragment of first-order logic."""
     nodes = make_nodes(opmap)
-
-    class Var(nodes.Terminal):
-        """A 0-ary variable.
-
-        Two cases:
-
-          - 0-ary function variable (integer or string variable)
-          - 0-ary propositional variable (atomic proposition)
-        """
-
-        def __init__(self, value):
-            super().__init__(value)
-            self.type = 'var'
-
-    class Bool(nodes.Terminal):
-        """A 0-ary connective."""
-
-        def __init__(self, value):
-            try:
-                value + 'a'
-            except TypeError:
-                raise TypeError(
-                    f'value must be string, got: {value}')
-            if value.lower() not in {'true', 'false'}:
-                raise TypeError(
-                    'value must be "true" or "false" '
-                    f'(case insensitive), got: {value}')
-            self.value = value.title()
-            self.type = 'bool'
-
-        def flatten(self, *arg, **kw):
-            return self.opmap[self.value]
-
-    class Num(nodes.Terminal):
-        """A 0-ary function."""
-
-        def __init__(self, value):
-            super().__init__(value)
-            self.type = 'num'
-
-    class Str(nodes.Terminal):
-        """A 0-ary function."""
-        # parser ensures that value has no quotes
-
-        def __init__(self, value):
-            super().__init__(value)
-            self.type = 'str'
-
-    class Comparator(nodes.Binary):
-        """Binary relational operator (2-ary predicate)."""
-
-    class Arithmetic(nodes.Binary):
-        """Binary function.
-
-        Maps terms to terms.
-        """
-
+    class Var(VarSpec, nodes.Terminal):
+        pass
+    class Bool(BoolSpec, nodes.Terminal):
+        pass
+    class Num(NumSpec, nodes.Terminal):
+        pass
+    class Str(StrSpec, nodes.Terminal):
+        pass
+    class Comparator(ComparatorSpec, nodes.Binary):
+        pass
+    class Arithmetic(ArithmeticSpec, nodes.Binary):
+        pass
     nodes.Var = Var
     nodes.Bool = Bool
     nodes.Num = Num
