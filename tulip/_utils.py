48a9e2cbc821897f1626def95b64656886302c2b
Ioannis Filippidis <jfilippidis@gmail.com>
2022-08-09 08:30:24 +0200

    ENH: add function `get_type()`

--- tulip/_utils.py
+++ tulip/_utils.py
@@ -18,3 +18,44 @@ def n_tuple(
         tuple, item_types)
     # for static analysis with `pytype`
     return generic
+
+
+def get_type(
+        maybe_module:
+            types.ModuleType |
+            None,
+        dotted_attr:
+            str
+        ) -> type:
+    """Return requested type if present.
+
+    If the module `maybe_module` has
+    been imported (is not `None`),
+    then return the requested type.
+
+    Otherwise, return `typing.Any`.
+
+    If the module *is* present,
+    but the requested type:
+
+    - does not exist:
+      raise `AttributeError`
+
+    - is not a `type`:
+      raise `TypeError`
+    """
+    if maybe_module is None:
+        return _ty.Any
+    module = maybe_module
+    path = dotted_attr.split('.')
+    attr = module
+    for step in path:
+        attr = getattr(attr, step)
+    if isinstance(attr, type):
+        return attr
+    raise TypeError(
+        'Expected designated '
+        'attribute to be a `type`, '
+        'but instead found: '
+        f'{type(attr) = }, '
+        f'{attr = }')
