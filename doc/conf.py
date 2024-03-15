e83833e480582ac38cac1480f78812d600450b76
Scott C. Livingston <slivingston@cds.caltech.edu>
2024-03-15 00:41:06 -0700

    DOC: fix layout error in sidebar
    If the sidebar contents are too wide, then the button that
    toggles hides/expands the sidebar is offset downward past the
    page footer. A CSS rule for the sidebar width can prevent this.

--- doc/conf.py
+++ doc/conf.py
@@ -127,6 +127,7 @@ html_theme_options = {"collapsiblesidebar":True}
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['patch.css']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
